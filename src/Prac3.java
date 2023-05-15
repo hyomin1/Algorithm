33238e81191c0a42d8e16a7eb488303545ad9559
leehm123456 <banana8967@naver.com>
2023-05-15 18:13:46 +0900

    구간 합 구하기(11659번) - 2

--- src/Prac3.java
+++ src/Prac3.java
@@ -3,36 +3,27 @@ import java.io.*;
 
 public class Prac3 {
     public static  void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int n, m;
-        n = sc.nextInt(); //수의 개수
-        m = sc.nextInt(); // 합을 구해야 하는 횟수
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in)); //많은 양의 데이터 읽을때는 Scanner보다 BufferReader
 
-        int a[] = new int[n];
-        int s[] = new int[n];
 
-        for(int k=0;k<n;k++) {
-            a[k] = sc.nextInt();
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine()); // ex)5 3 입력 n = 5 m =3
+
+        int n = Integer.parseInt(stringTokenizer.nextToken());
+        int m = Integer.parseInt(stringTokenizer.nextToken());
+        
+        long[]S = new long[n+1]; //덧셈 곱셈 많을경우 long형이 좋음
+
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine()); // ex) 5 4 3 2 1 입력
+        for(int i=1;i<=n;i++) {
+            S[i] = S[i-1]+ Integer.parseInt(stringTokenizer.nextToken());
         }
-
-        for (int k=0;k<n;k++) {
-            if(k == 0)
-                s[k] = a[k];
-            else
-                s[k] = s[k-1] + a[k];
+        for(int q=0;q<m;q++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            //구간 ex) 1 3 -> i = 1 j = 3
+            int i = Integer.parseInt(stringTokenizer.nextToken());
+            int j = Integer.parseInt(stringTokenizer.nextToken());
+            System.out.println(S[j]-S[i-1]);
         }
-
-        int i,j;
-        for(int k=0;k<m;k++) {
-            i = sc.nextInt();
-            j = sc.nextInt();
-            if (i==1)
-                System.out.println(s[j-1]);
-            else
-                System.out.println(s[j-1]-s[i-2]);
-
-        }
-
 
     }
 }
