e2c719064fac885b7a2071a6abb025efba14bd1c
hyomin1 <leehm123456@gmail.com>
2024-01-26 20:21:21 +0900

    백준 2750번(수 정렬하기, 버블정렬)

---
+++ src/Prac10.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Prac10 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+        for (int i = 0; i < N; i++){
+            A[i] = Integer.parseInt(br.readLine());
+        }
+        for(int i =0;i<N-1;i++) {
+            for(int j =0; j<N-i-1;j++) {
+                if (A[j] > A[j+1]) {
+                    int tmp = A[j+1];
+                    A[j+1] = A[j];
+                    A[j] = tmp;
+                }
+            }
+        }
+        for (int i : A) {
+            System.out.println(i);
+
+        }
+    }
+}
