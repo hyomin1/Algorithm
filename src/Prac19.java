0ecacf7f14f1f612c8a93b909634450bd8b70f0b
hyomin1 <leehm123456@gmail.com>
2024-01-29 17:59:32 +0900

    백준 11866번 (요세푸스 문제 0)

---
+++ src/Prac19.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Prac19 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i =1; i <= N; i++) {
+            queue.add(i);
+        }
+        System.out.print("<");
+        while(!queue.isEmpty()) {
+            for (int i = 0; i <K-1;i++) {
+                queue.add(queue.poll());
+            }
+            if(queue.size() == 1) {
+                System.out.print(queue.poll());
+            }
+            else {
+                System.out.print(queue.poll() + ", ");
+            }
+        }
+        System.out.println(">");
+    }
+}
