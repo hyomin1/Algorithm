74ef2bc9e954336f6f90e68dab2bbd17332266f7
hyomin1 <leehm123456@gmail.com>
2024-02-03 18:13:14 +0900

    백준 9012번 (괄호, 개선안)

--- src/baekjoon/P9012.java
+++ src/baekjoon/P9012.java
@@ -9,31 +9,38 @@ public class P9012 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
-
-        for(int i = 0; i<T;i++) {
+        for (int i = 0; i < T; i++) {
             char[] c = br.readLine().toCharArray();
             Stack<Character> stack = new Stack<>();
-            for(int j = 0; j<c.length;j++) {
-                stack.push(c[j]);
-                if(stack.size()>=2) {
-                    char c1 = stack.pop();
-                    char c2 = stack.pop();
-                    if(c1==c2) {
-                        stack.push(c2);
-                        stack.push(c1);
-                    }
-                    else if(c2==')' && c1 =='(') {
-                        stack.push(c2);
-                        stack.push(c1);
-                    }
+            boolean answer = isVPS(stack,c);
+            if(answer) {
+                    System.out.println("YES");
+            }
+                else {
+                    System.out.println("NO");
                 }
             }
-            if(stack.isEmpty()) {
-                System.out.println("YES");
+
+
+    }
+    static boolean isVPS(Stack stack,char[] c) {
+        for (int j = 0; j < c.length; j++) {
+            if (c[j] == '(') {
+                stack.push(c[j]);
+            } else {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                } else{
+                    return false;
+
+                }
             }
-            else {
-                System.out.println("NO");
-            }
+        }
+        if(!stack.isEmpty()) {
+            return false;
+        }
+        else {
+            return true;
         }
     }
 }
