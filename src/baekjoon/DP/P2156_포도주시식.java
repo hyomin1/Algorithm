ab902f69a5c38bf5ef766e06ada5dd469bbe4f2d
hyomin1 <leehm123456@gmail.com>
2024-03-11 16:11:05 +0900

    백준 2156번 (포도주 시식, DP)

---
+++ src/baekjoon/DP/P2156_포도주시식.java
@@ -0,0 +1,33 @@
+package baekjoon.DP;
+import java.io.*;
+
+public class P2156_포도주시식 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] A = new int[n+1];
+        int[] D = new int[n+1];
+
+        for(int i = 1; i <= n; i++) {
+            A[i] = Integer.parseInt(br.readLine());
+        }
+        D[1] = A[1];
+        if(n >=2) {
+            D[2] = D[1] + A[2];
+        }
+        for(int i = 3; i <=n; i++) {
+            D[i] = Math.max(D[i-1],Math.max(D[i-2] + A[i],D[i-3] + A[i-1] + A[i]));
+        }
+
+        int max = D[1];
+        for(int num : D) {
+            max = Math.max(num, max);
+        }
+        bw.write(max+"\n");
+        bw.flush();
+        bw.close();
+
+    }
+}
