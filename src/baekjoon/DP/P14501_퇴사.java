c691e6b485e502095a370ce50c1673a9d85d82a6
hyomin1 <leehm123456@gmail.com>
2024-03-16 17:36:49 +0900

    백준 14501번 (퇴사, DP)

---
+++ src/baekjoon/DP/P14501_퇴사.java
@@ -0,0 +1,39 @@
+package baekjoon.DP;
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class P14501_퇴사 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] D = new int[N+2];
+
+        int[] T = new int[N+2];
+        int[] P = new int[N+2];
+
+        for(int i = 1; i <= N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            T[i] = Integer.parseInt(st.nextToken());
+            P[i] = Integer.parseInt(st.nextToken());
+        }
+        int max = D[0];
+        for(int i = 1; i <= N + 1; i++) {
+            if(max < D[i]) {
+                max = D[i];
+            }
+            int next = i + T[i];
+            if(next > N+ 1) {
+                continue;
+            }
+            D[next] = Math.max(D[next], max + P[i]);
+        }
+
+        bw.write(D[N+1] + "\n");
+        bw.flush();
+        bw.close();
+
+    }
+}
