e0d88ca1994b7c340aa613951cbd01b4958bb51a
hyomin <98298940+hyomin1@users.noreply.github.com>
2024-03-26 00:19:53 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 2044 ms, Memory: 60568 KB -BaekjoonHub

---
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        
+
+        for(int i = 0; i<N;i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int num = st.countTokens();
+            for(int j =0;j<num;j++) {
+                StringBuffer sb = new StringBuffer(st.nextToken());
+                System.out.print(sb.reverse()+" ");
+            }
+            System.out.println();
+
+        }
+
+
+
+    }
+}
