3ffb3a8a5da570e40a3fc850fb05bd76b52c5547
hyomin <98298940+hyomin1@users.noreply.github.com>
2024-04-13 17:22:30 +0900

    [level 1] Title: 소수 찾기, Time: 25.06 ms, Memory: 71.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int[] A = new int[n+1];
+        for(int i = 2; i <=n; i++) {
+            A[i] = i;
+        }
+        for(int i = 2; i <=Math.sqrt(n); i++) {
+            if(A[i] == 0) continue;
+            for(int j = i + i; j <= n; j+=i) {
+                A[j] = 0;
+            }
+        }
+        for(int i = 2; i <=n; i++) {
+            if(A[i] != 0) answer++;
+        }
+        return answer;
+    }
+}
