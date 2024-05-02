e648eb472cc82efd1a81b40aca18b2cba8916430
hyomin <98298940+hyomin1@users.noreply.github.com>
2024-05-02 12:13:21 +0900

    [level 2] Title: 짝지어 제거하기, Time: 45.73 ms, Memory: 58.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution
+{
+    public int solution(String s)
+    {
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(!stack.isEmpty() && stack.peek() == c) {
+                stack.pop();
+            }
+            else {
+                stack.push(c);
+            }
+
+        }
+        int answer = stack.isEmpty() ? 1 : 0;
+
+
+        return answer;
+    }
+}
