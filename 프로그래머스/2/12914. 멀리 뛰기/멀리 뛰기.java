7293d321d745f8226b623727aeecc3da20024edb
hyomin <98298940+hyomin1@users.noreply.github.com>
2024-05-04 15:55:19 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.10 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public long solution(int n) {
+        long answer = 0;
+        int[] D = new int[n+1];
+        if (n == 1) return 1;
+        D[1] = 1;
+        D[2] = 2;
+        for(int i = 3 ; i <= n; i++) {
+            D[i] = (D[i-1] + D[i-2]) % 1234567;
+        }
+        answer = D[n];
+        return answer;
+    }
+}
