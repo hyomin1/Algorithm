1d46bdcdacf14dbe259a9c8f2ae9d24d5401dc2a
hyomin <98298940+hyomin1@users.noreply.github.com>
2024-06-02 15:16:28 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 120 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -0,0 +1,17 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        boolean[] visited = new boolean[31];
+        for(int i = 0; i < 28; i++) {
+            int num = Integer.parseInt(br.readLine());
+            visited[num] = true;
+        }
+        for(int i = 1; i <= 30; i++) {
+            if(!visited[i]) {
+                System.out.println(i);
+            }
+        }
+
+    }
+}
