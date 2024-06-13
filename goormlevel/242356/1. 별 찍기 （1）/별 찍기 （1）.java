e7fd8a4a0194489af4ceb90829ef913e51188934
hyomin <98298940+hyomin1@users.noreply.github.com>
2024-06-13 22:19:54 +0900

    [goormlevel] Title: 별 찍기 (1), Time: 0.08 ms, Memory: 31.55 MB, Difficulty: 1 -BaekjoonHub

---
+++ goormlevel/242356/1. 별 찍기 （1）/별 찍기 （1）.java
@@ -0,0 +1,13 @@
+import java.io.*;
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		for(int i = 1; i <= N; i++) {
+			for(int j = 0; j < i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
