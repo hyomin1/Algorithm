c6fc5a1a8695a1350a3d434ee1148fa0b1aa20f0
hyomin <98298940+hyomin1@users.noreply.github.com>
2024-07-02 16:48:30 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.08 ms, Memory: 82.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        String[] str = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        for(int i = 0; i < str.length; i++) {
+            s = s.replace(str[i], String.valueOf(i));
+        }
+        return Integer.parseInt(s);
+    }
+}
