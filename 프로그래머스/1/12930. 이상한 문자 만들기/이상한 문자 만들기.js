2ee8f13feb1aae8f72cb87a5c51af104d35f002f
hyomin <leehm123456@gmail.com>
2025-03-12 15:15:12 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.25 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,12 +1,15 @@
 function solution(s) {
-    var answer = '';
+    let answer = '';
+  
     const arr = s.split(' ');
     for(let i = 0; i < arr.length; i++) {
         for(let j = 0; j < arr[i].length; j++) {
-            if(j % 2 == 0) answer += arr[i][j].toUpperCase();
-            else answer += arr[i][j].toLowerCase();
+            if (j % 2 === 0) {
+                answer += arr[i][j].toUpperCase();
+            } else answer += arr[i][j].toLowerCase();
         }
-        if(i !== arr.length-1) answer += " ";
+        if (i < arr.length -1) answer += ' ';
+       
     }
     
     return answer;
