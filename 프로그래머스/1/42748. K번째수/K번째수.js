bd87f9fee5b3f34d29ca34b2a429478fc0643f2b
hyomin <leehm123456@gmail.com>
2025-03-12 14:25:37 +0900

    [level 1] Title: K번째수, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -0,0 +1,8 @@
+function solution(array, commands) {
+    let answer = [];
+    for(let i = 0; i < commands.length; i++) {
+        const newArr = array.slice(commands[i][0]-1,commands[i][1]).sort((a,b)=>a-b);
+        answer.push(newArr[commands[i][2]-1])
+    }
+    return answer;
+}
