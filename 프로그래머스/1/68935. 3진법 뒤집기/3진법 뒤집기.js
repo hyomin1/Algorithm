18eca346c26aab7bd6bb6d6ae02199ab477eabb9
hyomin <leehm123456@gmail.com>
2025-03-13 01:45:10 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,13 +1,9 @@
 function solution(n) {
     let answer = 0;
     const arr = [];
-    while (n > 2) {
-        arr.push(n % 3);
-        n = Math.floor(n /3);
-    }
-    arr.push(n);
-    arr.reverse();
-    const str = arr.join('');
+   
+    const str = n.toString(3);
+
     for (let i = 0; i < str.length; i++) {
         answer += parseInt(str[i]) * (3 ** i);
     }
