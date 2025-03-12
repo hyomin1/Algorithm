a9a010f97710d7ea264648413011ca4b2eaef2d2
hyomin <leehm123456@gmail.com>
2025-03-12 14:40:08 +0900

    [level 1] Title: 시저 암호, Time: 3.09 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/README.md
+++ 프로그래머스/1/12926. 시저 암호/README.md
@@ -1,10 +1,10 @@
 # [level 1] 시저 암호 - 12926 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12926?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12926) 
 
 ### 성능 요약
 
-메모리: 37.3 MB, 시간: 38.10 ms
+메모리: 36.9 MB, 시간: 3.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 27일 14:42:16
+2025년 03월 12일 14:40:24
 
 ### 문제 설명
 
--- 프로그래머스/1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -1,18 +1,25 @@
+function changeCharCode(c,n, number) {
+    
+    c = c.charCodeAt(c) + n;
+    c = (c - number) % 26 + number;
+    return String.fromCharCode(c);
+}
+
 function solution(s, n) {
     var answer = '';
     for(let i = 0; i < s.length; i++) {
         let c = s.charAt(i);
         if(c >= 'a' && c <= 'z') {
-            c = c.charCodeAt(c) + n;
-            c = (c-97) % 26 + 97;
-            answer += String.fromCharCode(c);
-        } 
-        else if (c >='A' && c <='Z') {
-            c = c.charCodeAt(c) + n;
-            c = (c-65) % 26 + 65;
-            answer += String.fromCharCode(c);
+           answer += changeCharCode(c,n,97);
         }
-        else answer += c;
+        else if(c >='A' && c <='Z') {
+            answer += changeCharCode(c,n,65);
+        }
+        else {
+            answer += c;
+        }
+      
+        
     }
     return answer;
 }
