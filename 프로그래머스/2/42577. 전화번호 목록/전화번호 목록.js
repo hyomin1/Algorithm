a95bc0d501a0ed1131a479a9295c179ed63a0a27
hyomin <leehm123456@gmail.com>
2025-03-19 11:52:33 +0900

    [level 2] Title: 전화번호 목록, Time: 131.82 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,15 +1,9 @@
 function solution(phone_book) {
     var answer = true;
     const map = {};
-    for (let i = 0; i < phone_book.length; i++) {
-        let str = '';
-        for (let j = 0; j < phone_book[i].length - 1; j++) {
-            str += phone_book[i][j];
-            map[str] = phone_book[i];
-        }
-    }
-    for (let i = 0; i < phone_book.length; i++) {
-        if (map[phone_book[i]]) {
+    phone_book.sort();
+    for(let i = 0; i < phone_book.length - 1; i++) {
+        if(phone_book[i+1].startsWith(phone_book[i])) {
             answer = false;
             break;
         }
