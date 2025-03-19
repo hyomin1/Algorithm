92fdcc75fc8805a6623fad6308e67f0eed2afde4
hyomin <leehm123456@gmail.com>
2025-03-19 14:57:56 +0900

    [level 2] Title: 소수 찾기, Time: 6.85 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
@@ -1,7 +1,7 @@
 function isPrime(n) {
     if (n < 2) return false;
     const sq = Math.floor(Math.sqrt(n));
-    for(let i = 2; i <= sq; i++) {
+    for (let i = 2 ; i <= sq; i++) {
         if (n % i === 0) return false;
     }
     return true;
@@ -9,23 +9,19 @@ function isPrime(n) {
 
 function solution(numbers) {
     var answer = 0;
-    const nums = numbers.split('');
-    const visited = new Array(nums.length).fill(false);
+    const visited = [];
     const set = new Set();
-    
-    dfs(nums, visited, '', set);
-    return set.size;
-}
-
-function dfs(nums, visited, curNum, set) {
-    const num = Number(curNum);
-    if (num > 0 && isPrime(num)) set.add(num);
-    
-    for (let i = 0; i < nums.length; i++) {
-        if(!visited[i]) {
-            visited[i] = true;
-            dfs(nums,visited,curNum + nums[i],set);
-            visited[i] = false;
+    function dfs(cur) {
+        const num = Number(cur);
+        if (isPrime(num)) set.add(num);
+        for (let i = 0; i < numbers.length; i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                dfs(num + numbers[i]);
+                visited[i] = false;
+            }
         }
     }
+    dfs("")
+    return set.size;
 }
