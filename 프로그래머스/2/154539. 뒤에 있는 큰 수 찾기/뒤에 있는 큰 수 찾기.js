805e83cab2f6d5339fb8cdbb580c5164433c6e02
hyomin <leehm123456@gmail.com>
2025-03-20 13:33:43 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 182.98 ms, Memory: 156 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
@@ -5,12 +5,12 @@ function solution(numbers) {
     
     for (let i = 1; i < numbers.length; i++) {
         
-        while(stack.length !== 0 && numbers[stack[stack.length - 1]] < numbers[i]) {
+        while(stack.length && numbers[stack[stack.length - 1]] < numbers[i]) {
             answer[stack.pop()] = numbers[i];
         }
         stack.push(i);
     }
-    while(stack.length !== 0) {
+    while(stack.length) {
         answer[stack.pop()] = -1;
     }
     return answer;
