2d57416522cefb51ce798c41d765131fb380cb48
hyomin <leehm123456@gmail.com>
2025-03-21 17:35:46 +0900

    [level 2] Title: 소수 찾기, Time: 7.14 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
@@ -1,27 +1,27 @@
-function isPrime(n) {
-    if (n < 2) return false;
-    const sq = Math.floor(Math.sqrt(n));
-    for (let i = 2 ; i <= sq; i++) {
-        if (n % i === 0) return false;
+function isPrime(num) {
+    if (num < 2) return false;
+    const sq = Math.floor(Math.sqrt(num));
+    for(let i = 2; i <= sq; i++) {
+        if (num % i === 0) return false;
     }
     return true;
 }
 
-function solution(numbers) {
-    var answer = 0;
-    const visited = [];
-    const set = new Set();
-    function dfs(cur) {
-        const num = Number(cur);
-        if (isPrime(num)) set.add(num);
-        for (let i = 0; i < numbers.length; i++) {
-            if(!visited[i]) {
-                visited[i] = true;
-                dfs(num + numbers[i]);
-                visited[i] = false;
-            }
+function dfs(number, depth, numbers, set, visited) {
+    if (isPrime(Number(number))) set.add(Number(number));
+    for (let i = 0; i < numbers.length; i++) {
+        if(!visited[i]) {
+            visited[i] = true;
+            dfs(number + numbers[i], depth+1, numbers, set, visited);
+            visited[i] = false;
         }
     }
-    dfs("")
+}
+
+function solution(numbers) {
+    var answer = 0;
+    const set = new Set();
+    const visited = Array(numbers.length).fill(false);
+    dfs("",0,numbers,set,visited);
     return set.size;
 }
