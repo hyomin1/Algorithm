75ae0cf9adee5821b2a848b8aa44b1e07419d5b1
hyomin <leehm123456@gmail.com>
2025-03-21 16:21:51 +0900

    [level 3] Title: 단어 변환, Time: 0.74 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -1,32 +1,33 @@
 function checkWord(word1, word2) {
     let diffCount = 0;
     for (let i = 0; i < word1.length; i++) {
-        if(word1[i] !== word2[i]) diffCount++;
-        if (diffCount > 1) return false;
+        if(word1[i] !== word2[i]) {
+            diffCount++;
+        }
     }
-    return diffCount === 1 ? true : false;
+    return diffCount === 1;
 }
 
 function solution(begin, target, words) {
-    let answer = 0;
-    if (!words.includes(target)) return answer;
-    
-    const queue = [[begin,0]];
-    const visited = new Set([begin]);
+    var answer = 0;
+    // 짧은 경로 -> BFS
+    const queue = [];
+    const set = new Set(); // visited 
+    set.add(begin);
+    queue.push([begin,0]);
     
     while (queue.length > 0) {
-        const [currentWord,step] = queue.shift();
-        
-        if (currentWord === target) return step;
-        
-        for (const word of words) {
-            if(!visited.has(word) && checkWord(currentWord,word)) {
-               queue.push([word,step+1]);
-               visited.add(word);
-             }
+        const [currentWord, step] = queue.shift();
+        if (currentWord === target) {
+            return step;
         }
+        for (const word of words) {
+            if(!set.has(word) && checkWord(currentWord,word)) {
+                set.add(word);
+                queue.push([word,step+1]);
+            }
+        }
+        
     }
-    
-    
     return answer;
 }
