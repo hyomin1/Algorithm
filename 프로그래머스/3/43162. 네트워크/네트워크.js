865a05f337019c66b23f30f48e87f1b1baf18e3b
hyomin <leehm123456@gmail.com>
2025-03-21 16:01:09 +0900

    [level 3] Title: 네트워크, Time: 0.60 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,33 +1,31 @@
+function dfs(currentNode, graph, visited) {
+    visited[currentNode] = true;
+    if (!graph[currentNode]) return;
+    for (const nextNode of graph[currentNode]) {
+        if (!visited[nextNode]) {
+            visited[nextNode] = true;
+            dfs(nextNode, graph, visited);
+        }
+    }
+}
+
 function solution(n, computers) {
     var answer = 0;
     const graph = {};
-    for (let i = 0; i < computers.length; i++) {
-        graph[i] = [];
-    }
-    for (let i = 0; i < computers.length; i++) {
-        for (let j = 0; j < computers[i].length; j++) {
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
             if (i !== j && computers[i][j] === 1) {
-                graph[i].push(j);
+                if (graph[i]) graph[i].push(j);
+                else graph[i] = [j];
             }
         }
     }
-    const visited = new Array(n).fill(false);
-    
+    const visited = Array(n).fill(false);
     for (let i = 0; i < n; i++) {
         if(!visited[i]) {
-            visited[i] = true;
             answer++;
-            dfs(i,visited,graph);
+            dfs(i,graph,visited);
         }
     }
-    
     return answer;
-}
-
-function dfs(node,visited,graph) {
-    visited[node] = true;
-    
-    for (const n of graph[node]) {
-        if(!visited[n]) dfs(n,visited,graph);
-    }
 }
