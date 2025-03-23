a520bf12cb7a8b8d268bbe42f8a960cc2713b38b
hyomin <leehm123456@gmail.com>
2025-03-23 15:55:04 +0900

    [level 1] Title: 행렬의 덧셈, Time: 8.99 ms, Memory: 67.9 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,7 +1,7 @@
 function solution(arr1, arr2) {
-    var answer = [];
-    arr1.forEach((_,i) => answer[i] = []);
+    var answer = [[]];
     for (let i = 0; i < arr1.length; i++) {
+        answer[i] = [];
         for (let j = 0; j < arr1[i].length; j++) {
             answer[i][j] = arr1[i][j] + arr2[i][j];
         }
