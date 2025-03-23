f9085694616c33c8cc6da78107d20ba55c3808e3
hyomin <leehm123456@gmail.com>
2025-03-23 15:45:45 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,9 +1,6 @@
 function solution(phone_number) {
     var answer = '';
     
-    for (let i = 0; i < phone_number.length; i++) {
-        if (i < phone_number.length - 4) answer += '*';
-        else answer += phone_number[i];
-    }
+    answer = '*'.repeat(phone_number.length - 4) + phone_number.slice(-4);
     return answer;
 }
