bed17f27ce70047a311827d414744fb600b93fcd
hyomin <leehm123456@gmail.com>
2025-03-26 14:40:22 +0900

    [level 2] Title: 짝지어 제거하기, Time: 51.38 ms, Memory: 50.8 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/README.md
+++ 프로그래머스/2/12973. 짝지어 제거하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 짝지어 제거하기 - 12973 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12973?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12973) 
 
 ### 성능 요약
 
-메모리: 58.9 MB, 시간: 45.73 ms
+메모리: 50.8 MB, 시간: 51.38 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 02일 12:13:19
+2025년 03월 26일 14:40:20
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,19 @@
+function solution(s)
+{
+    var answer = 0;
+    const stack = [];
+
+    for (let i = 0; i < s.length; i++) {
+        if (stack.length === 0) {
+            stack.push(s[i]);
+        } else {
+            if (stack[stack.length - 1] === s[i]) stack.pop();
+            else stack.push(s[i]);
+        }
+
+    }
+    answer = stack.length === 0 ? 1 : 0;
+    
+
+    return answer;
+}
