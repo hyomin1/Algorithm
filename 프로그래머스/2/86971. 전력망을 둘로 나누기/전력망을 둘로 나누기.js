a5cb0ef3323a3162dc63140f9f5fcae2907035df
hyomin <leehm123456@gmail.com>
2025-04-04 18:19:02 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 30.93 ms, Memory: 38.6 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
@@ -1,10 +1,10 @@
 function dfs(node, graph, visited) {
-    visited[node] = true;
+    visited.add(node);
     let count = 1;
-    for (const next of graph[node]) {
-        if (!visited[next]) {
-            visited[next] = true;
-            count += dfs(next, graph, visited);
+    for(const neighbor of graph[node] || []) {
+        if(!visited.has(neighbor)) {
+            visited.add(neighbor);
+            count += dfs(neighbor,graph,visited);
         }
     }
     return count;
@@ -12,24 +12,22 @@ function dfs(node, graph, visited) {
 
 function solution(n, wires) {
     var answer = Infinity;
-    const graph = {};
+    
     
     for (let i = 0; i < wires.length; i++) {
-        for (let j = 1; j <= n; j++) {
-            graph[j] = [];
-        }
+        const visited = new Set();
+        const graph = {};
+        for (let j = 1; j <= n; j++) graph[j] = [];
         for (let j = 0; j < wires.length; j++) {
-            if (i !== j) {
-                const [n1, n2] = wires[j];
-                graph[n1].push(n2);
-                graph[n2].push(n1);
-            }
+            if (i === j) continue;
+            const [u,v] = wires[j];
+            graph[u].push(v);
+            graph[v].push(u);
         }
-        const visited = Array(n+1).fill(false);
-        const first = dfs(1,graph,visited);
-        const second = n - first;
-        answer = Math.min(answer,Math.abs(first-second));
-        
+         const first = dfs(1,graph,visited);
+            const second = n - first;
+            answer = Math.min(answer, Math.abs(first-second));
+
     }
     return answer;
 }
