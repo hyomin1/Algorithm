ae9102603e5bc62b355a1018299c961ef6b3918d
hyomin <leehm123456@gmail.com>
2025-04-05 17:16:13 +0900

    [level 2] Title: 모음 사전, Time: 0.63 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -1,16 +1,17 @@
-function dfs(word,depth,dict,words) {
-    dict.push(word);
-    if(depth === words.length) return;
-    for(let i = 0; i < words.length; i++) {
-        dfs(word + words[i],depth+1,dict,words);
-    }
-}
-
 function solution(word) {
     var answer = 0;
+    const map = [];
     const words = ['A','E','I','O','U'];
-    const dict = [];
-    dfs("",0,dict,words);
-    answer = dict.indexOf(word);
+    function dfs(w,depth) {
+        map.push(w);
+        if(depth === words.length) {
+            return;
+        }
+        for (let i = 0; i < words.length; i++) {
+            dfs(w + words[i],depth + 1);
+        }
+    }
+    dfs('',0);
+    answer = map.indexOf(word);
     return answer;
 }
