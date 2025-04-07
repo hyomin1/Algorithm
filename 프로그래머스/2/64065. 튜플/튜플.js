c463aebc53a5dc7dd1100410e3ee067580999d2b
hyomin <leehm123456@gmail.com>
2025-04-07 15:47:08 +0900

    [level 2] Title: 튜플, Time: 38.72 ms, Memory: 48.7 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.js
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -1,15 +1,14 @@
 function solution(s) {
     var answer = [];
-    s = s.replaceAll(/\{/g,'');
-    s = s.replaceAll(/\}/g,'');
-    const arr = s.split(',').map(Number);
+    s = s.replaceAll('{','');
+    s = s.replaceAll('}','');
+    s = s.split(',').map((v)=>Number(v));
     const map = {};
-    for(let i = 0; i < arr.length; i++) {
-        const n = arr[i];
-        map[n] = (map[n] || 0) + 1;
+    for (let i = 0; i < s.length; i++) {
+        const num = s[i];
+        map[num] = (map[num] || 0) + 1;
     }
-    const entries = Object.entries(map);
-    entries.sort((a,b) => b[1] - a[1]);
-    const sortValue = entries.map((v) => Number(v[0]));
-    return sortValue;
+    answer = Object.entries(map).sort((a,b) => b[1] - a[1]);
+    answer = answer.map((v) => Number(v[0]));
+    return answer;
 }
