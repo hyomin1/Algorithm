c10664ec097b0e0e47d6d30595b3ee4600d3f476
hyomin <leehm123456@gmail.com>
2025-04-08 15:20:07 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 71.02 ms, Memory: 44.6 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
@@ -1,20 +1,37 @@
-function parseFile(file) {
-    const head = file.match(/^[^0-9]*/)[0];
-    const number = file.substring(head.length).match(/\d{1,5}/)[0];
-    return {
-        file,
-        head:head.toLowerCase(),
-        number:Number(number),
+function makeHead(str) {
+    let head = '';
+    let index = 0;
+    for(let i = 0; i < str.length; i++) {
+        if (str[i] >= '0' && str[i] <= '9') {
+            index = i;
+            break;
+        }
+        head += str[i];
     }
+    return [head.toLowerCase(),index];
+}
+
+function makeNumber(str,index) {
+    let count = 0;
+    let number = '';
+    for (let i = index; i < str.length; i++) {
+        if (count === 5 || str[i] < '0' || str[i] > '9') break;
+        if (str[i] >= '0' && str[i] <= '9') {
+            count++;
+            number += str[i];
+        }
+    }
+    return Number(number);
 }
 
 function solution(files) {
-    var answer = [];
-    files = files.map((file) => parseFile(file));
     files.sort((a,b) => {
-        if(a.head === b.head) return a.number - b.number;
-        return a.head.localeCompare(b.head);
-           })
-    answer = files.map((f)=>f.file);
-    return answer;
+        const [h1,index1] = makeHead(a);
+        const [h2,index2] = makeHead(b);
+        const n1 = makeNumber(a,index1);
+        const n2 = makeNumber(b,index2);
+        if (h1 === h2) return n1 - n2;
+        return h1.localeCompare(h2);
+    });
+    return files;
 }
