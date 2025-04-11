9507e15416d7f1003f25499eca200dda659ceb0f
hyomin <leehm123456@gmail.com>
2025-04-11 16:35:10 +0900

    [level 2] Title: 피보나치 수, Time: 4.64 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -1,10 +1,7 @@
 function solution(n) {
-    var answer = 0;
-    const dp = Array(n+1).fill(0);
-    dp[0] = 0;
-    dp[1] = 1;
-    for (let i = 2; i < dp.length;i++) {
-        dp[i] = (dp[i-2] + dp[i-1]) % 1234567;
+    const dp = [0,1];
+    for (let i = 2; i <= n ;i++) {
+        dp.push((dp[i-2] + dp[i-1]) % 1234567);
     }
     return dp[n];
 }
