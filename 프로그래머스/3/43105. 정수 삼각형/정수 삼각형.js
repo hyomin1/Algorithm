d1c76e9af653b232e6a6d86f4d39c9942792c40f
hyomin <leehm123456@gmail.com>
2025-04-14 15:53:23 +0900

    [level 3] Title: 정수 삼각형, Time: 25.65 ms, Memory: 46.5 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -1,16 +1,18 @@
 function solution(triangle) {
     var answer = 0;
-    const arr = [];
+    const dp = Array(triangle.length).fill().map((_,i) => Array(triangle[i].length).fill(0));
+    dp[0][0] = triangle[0][0];
     for (let i = 1; i < triangle.length; i++) {
         for (let j = 0; j < triangle[i].length; j++) {
             if (j === 0) {
-                triangle[i][j] += triangle[i-1][j];
+                dp[i][j] = dp[i-1][j] + triangle[i][j];
+            } else if (j === triangle[i].length -1){
+                dp[i][j] = dp[i-1][j-1] + triangle[i][j];
             } else {
-                triangle[i][j] += Math.max((triangle[i-1][j] || 0),triangle[i-1][j-1] || 0); 
+                dp[i][j] = Math.max(dp[i-1][j] + triangle[i][j], dp[i-1][j-1] + triangle[i][j], dp[i][j]);
             }
-            
         }
     }
-    answer = Math.max(...triangle[triangle.length - 1]);
+    answer = Math.max(...dp.flatMap((v) => v));
     return answer;
 }
