dd0163c3bccb8ec2c4bc5f2293a519e0bb402aed
hyomin <leehm123456@gmail.com>
2025-04-15 15:29:11 +0900

    [level 1] Title: 예산, Time: 0.25 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.js
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -1,13 +1,11 @@
 function solution(d, budget) {
-    let answer = 0;
+    var answer = 0;
     d.sort((a,b) => a - b);
-    let sum = 0;
-    for (let i = 0; i < d.length; i++) {
-        if (d[i] > budget) break;
-        budget -= d[i];
-        answer++;
+    for (const cost of d) {
+        if (budget >= cost) {
+            budget -= cost;
+            answer++;
+        }
     }
-   
-    
     return answer;
 }
