c11ffb204eff88fbd3e1ed77ca4d233e07784663
hyomin <leehm123456@gmail.com>
2025-04-15 15:48:43 +0900

    [level 2] Title: 귤 고르기, Time: 1918.33 ms, Memory: 59.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -0,0 +1,17 @@
+function solution(k, tangerine) {
+    var answer = 0;
+    const arr = [];
+    arr[0] = 0;
+    for (const t of tangerine) {
+        arr[t] = (arr[t] || 0) + 1;   
+    }
+    
+    arr.sort((a,b) => b - a);
+    
+    for (const a of arr) {
+        k -= a;
+        answer++;
+        if (k <= 0) break; 
+    }
+    return answer;
+}
