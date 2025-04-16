fd14df66a23a64d56b0d7e0781e7e01bd900c721
hyomin <leehm123456@gmail.com>
2025-04-16 15:21:57 +0900

    [level 1] Title: 체육복, Time: 0.29 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,11 +1,12 @@
 function solution(n, lost, reserve) {
     var answer = 0;
-    const students = new Array(n+1).fill(1);
+    const students = Array(n+1).fill(1);
     lost.forEach((l) => students[l]--);
     reserve.forEach((r) => students[r]++);
-    for (let i = 1; i <= n; i++) {
-        if(students[i] === 0) {
-            if(students[i-1] === 2) {
+    
+    for (let i = 1; i <= students.length; i++) {
+        if (students[i] === 0) { // 체육복 없는 경우
+            if (students[i-1] === 2) { // 앞 학생 여벌 있는 경우
                 students[i-1]--;
                 students[i]++;
             } else if (students[i+1] === 2) {
