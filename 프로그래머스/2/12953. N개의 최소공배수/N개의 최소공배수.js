2a8b08cb31e5ef0c66f437258e8459bef8996d3d
hyomin <leehm123456@gmail.com>
2025-04-29 17:00:40 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -1,15 +1,12 @@
 function getGcd(a,b) {
-    if (b === 0) return a;
-    return getGcd(b, a % b);
+    return b === 0 ? a : getGcd(b, a % b);
+}
+
+function getLcm(a,b) {
+    return (a * b) / getGcd(a,b);
 }
 
 function solution(arr) {
-    if (arr.length === 1) return arr[0];
-    let lcm = arr[0] * arr[1] / getGcd(arr[0],arr[1]);
-    if (arr.length === 2) return lcm;
-    for (let i = 2; i < arr.length; i++) {
-        lcm = lcm * arr[i] / getGcd(lcm,arr[i]);
-        
-    }
-    return lcm;
+    return arr.reduce((acc,cur) => getLcm(acc,cur));
+    
 }
