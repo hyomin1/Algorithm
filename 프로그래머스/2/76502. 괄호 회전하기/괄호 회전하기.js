6b0d824753d2f947c04bdbbdd3b0cb7dd7135367
hyomin <leehm123456@gmail.com>
2025-05-01 16:43:22 +0900

    [level 2] Title: 괄호 회전하기, Time: 12.76 ms, Memory: 37.3 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,31 +1,29 @@
 function solution(s) {
     var answer = 0;
-    const n = s.length;
-    
-    for (let i = 0; i < n; i++) {
+    const len = s.length;
+    for (let i = 0; i < len; i++) {
+        let checked = true;
         const stack = [];
-        let isCorrect = true;
-        for (let j = 0; j < n; j++) {
-            const c = s[(i+j) % n];
+        for (let j = 0; j < len; j++) {     
+            const c = s[(i+j) % len];
             if (c === '(' || c === '{' || c === '[') {
                 stack.push(c);
             } else {
                 if (stack.length === 0) {
-                    isCorrect = false;
+                    checked = false;
                     break;
+                } else {
+                    const top = stack[stack.length - 1];
+                    if (c ===')' && top === '(') stack.pop();
+                    else if (c === '}' && top === '{') stack.pop();
+                    else if (c ===']' && top === '[') stack.pop();
                 }
-                
-                const top = stack[stack.length - 1];
-                if (c === ']' && top === '[') stack.pop();
-                else if (c === '}' && top === '{') stack.pop();
-                else if (c === ')' && top === '(') stack.pop();
-                else {
-                    isCorrect = false;
-                    break;
-                }
-             }
+            }
         }
-        if (isCorrect && stack.length === 0) answer++;
+        if (checked && stack.length === 0) {
+            answer++;
+        }
+            
     }
     return answer;
 }
