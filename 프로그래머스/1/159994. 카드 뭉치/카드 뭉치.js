bde517a785e10ca1f8dc96f39da772a573e740d7
hyomin <leehm123456@gmail.com>
2025-05-03 16:08:26 +0900

    [level 1] Title: 카드 뭉치, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,15 +1,15 @@
 function solution(cards1, cards2, goal) {
     var answer = '';
     let check = true;
-    for(let i = 0; i < goal.length; i++) {
-        const v = goal[i];
-        if (cards1[0] === v) cards1.shift();
-        else if (cards2[0] === v) cards2.shift();
+    for (const word of goal) {
+        if (cards1[0] === word) cards1.shift();
+        else if (cards2[0] === word) cards2.shift();
         else {
             check = false;
             break;
         }
     }
-    return check ? 'Yes' : 'No';
-    
+   
+    answer = check ? 'Yes' : 'No'
+    return answer;
 }
