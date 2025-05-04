88984b46a8da70f1f58ebfadfc42110cce72718c
hyomin <leehm123456@gmail.com>
2025-05-04 15:47:10 +0900

    [level 1] Title: 신고 결과 받기, Time: 291.46 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -1,28 +1,26 @@
 function solution(id_list, report, k) {
     var answer = [];
-    const reportMap = {}; // 신고자, 신고 한 사람 목록
-    const countMap = {}; // 신고 받은사람 count
-    for (let i = 0; i < report.length; i++) {
-        const [reporter, reported] = report[i].split(' ');
-        if (!reportMap[reporter]) reportMap[reporter] = [];
-        if (!reportMap[reporter].includes(reported)) {
-            reportMap[reporter] = reportMap[reporter].concat(reported);
-            countMap[reported] = (countMap[reported] || 0) + 1;
-
-        }
+    const map = {};
+    const count = {};
+    for (const id of id_list) {
+        map[id] = [];
+        count[id] = 0;
     }
-    //console.log(reportMap,countMap);
-    for (let i = 0; i < id_list.length; i++) {
-        const reporter = id_list[i];
-        let count = 0;
-
-        if (reportMap[reporter]) {
-            for (let j = 0; j < reportMap[reporter].length; j++) {
-                const reported = reportMap[reporter][j];
-                if(countMap[reported] >= k) count++;
-            }
-        }
-        answer.push(count);
+    for (const r of report) {
+        const [a, b] = r.split(' ');
+        if(!map[a].includes(b)) map[a].push(b);
+    }
+    
+    const keys = Object.keys(map);
+    for (const key of keys) {
+        map[key].forEach((v) => count[v]++);
+    }
+    for (const key of keys) {
+        let i = 0;
+        map[key].forEach((v) => {
+            if (count[v] >= k) i++;
+        })
+        answer.push(i);
     }
     return answer;
 }
