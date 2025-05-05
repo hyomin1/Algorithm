8897310b9563faa8d1b11e1ce6888eb618c4581f
hyomin <leehm123456@gmail.com>
2025-05-05 18:29:42 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.92 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -1,20 +1,20 @@
+function getDay(date) {
+    const [y,m,d] = date.split('.').map(Number);
+    return y * 12 * 28 + m * 28 + d;
+}
+
 function solution(today, terms, privacies) {
     var answer = [];
-    const term = {};
+    const termMap = {};
     for (const t of terms) {
         const [type, month] = t.split(' ');
-        term[type] = Number(month);
+        termMap[type] = Number(month) * 28;
     }
-    const [todayYear, todayMonth, todayDate] = today.split('.').map(Number);
-    for (let i = 0; i < privacies.length; i++) {
-        const [day, type] = privacies[i].split(' ');
-        let [year,month,date] = day.split('.').map(Number);
-        const sum = month + term[type];
-        year += Math.floor(sum / 12);
-        month = sum % 12;
-        date = (date - 1) % 28;
-        if (new Date(todayYear,todayMonth,todayDate).getTime() > new Date(year,month,date).getTime()) answer.push(i+1);
-        
-    }
+    const todayDays = getDay(today);
+    privacies.forEach((v,i) => {
+        const [day, type] = v.split(' ');
+        const days = getDay(day) + termMap[type];
+        if (todayDays >= days) answer.push(i+1);
+    })
     return answer;
 }
