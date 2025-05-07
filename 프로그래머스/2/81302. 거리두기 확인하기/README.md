6b9940594a4f65515e64c6f20e865494ce0fa271
hyomin <leehm123456@gmail.com>
2025-05-07 17:52:35 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.68 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/README.md
+++ 프로그래머스/2/81302. 거리두기 확인하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 거리두기 확인하기 - 81302 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81302) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81302#fn1) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.75 ms
+메모리: 33.7 MB, 시간: 0.68 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 07일 17:47:42
+2025년 05월 07일 17:52:34
 
 ### 문제 설명
 
--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.js
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.js
@@ -1,47 +1,44 @@
 function solution(places) {
     var answer = [];
-    const dx = [0,0,1,-1];
-    const dy = [1,-1,0,0];
-    for (let i = 0; i < places.length; i++) {
+    
+    for (const place of places) {
         const person = [];
-        for (let j = 0; j < places[i].length; j++) {
-            for (let k = 0; k < places[i][j].length; k++) {
-                if (places[i][j][k] === 'P') person.push([j,k]);
+        for (let i = 0; i < place.length; i++) {
+            for (let j = 0; j < place[i].length; j++) {
+                if (place[i][j] === 'P') person.push([i,j]);
             }
         }
-        let check = true;
-        for (let j = 0; j < person.length - 1; j++) {
-            const [y1,x1] = person[j];
-            for (let k = j + 1; k < person.length; k++) {
-                const [y2,x2] = person[k];
-                const m = Math.abs(y2-y1) + Math.abs(x2-x1);
-                if (m === 1) { // 맨해튼 거리 1, 지키지 않음
-                    check = false;
-                    break;
-                } else if (m === 2) { // 맨해튼 거리 2
+        
+        let valid = true;
+        
+        for (let i = 0; i < person.length - 1; i++) {
+            const [y1,x1] = person[i];
+            
+            for (let j = i + 1; j < person.length; j++) {
+                const [y2,x2] = person[j];
+                const dist = Math.abs(y1-y2) + Math.abs(x1-x2);
+                if (dist === 1) { // 맨해튼 거리 1, 지키지 않음
+                    valid = false;
+                } else if (dist === 2) { // 맨해튼 거리 2
                     if (y1 === y2) { // 같은 행
                         const midX = (x1 + x2) / 2;
-                        if (places[i][y1][midX] !== 'X') {
-                            check = false;
-                            break;
+                        if (place[y1][midX] !== 'X') {
+                            valid = false;
                         }
                     } else if (x1 === x2) { // 같은 열
                         const midY = (y1 + y2) / 2;
-                        if (places[i][midY][x1] !== 'X') {
-                            check = false;
-                            break;
+                        if (place[midY][x1] !== 'X') {
+                            valid = false;
                         }
                     } else { // 대각선
-                        if (places[i][y1][x2] !== 'X' || places[i][y2][x1] !== 'X') {
-                            check = false;
-                            break;
+                        if (place[y1][x2] !== 'X' || place[y2][x1] !== 'X') {
+                            valid = false;
                         }
                     }
                 }
             }
         }
-        if (check) answer.push(1);
-        else answer.push(0);
+        answer.push(valid ? 1 : 0);    
     }
     
     return answer;
