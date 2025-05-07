84dac4cfb0bb5f12f982d152739b9af11af479a8
hyomin <leehm123456@gmail.com>
2025-05-07 18:41:55 +0900

    [level 3] Title: 숫자 게임, Time: 87.33 ms, Memory: 43.6 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.js
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.js
@@ -1,12 +1,12 @@
 function solution(A, B) {
     var answer = 0;
-    A.sort((a,b)=>a-b);
-    B.sort((a,b)=>a-b);
+    A.sort((a,b) => a - b);
+    B.sort((a,b) => a - b);
     let index = 0;
     for (let i = 0; i < B.length; i++) {
-        if(B[i] > A[index]) {
-            answer++;
+        if (B[i] > A[index]) {
             index++;
+            answer++;
         }
     }
     return answer;
