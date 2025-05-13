7a5f9d2e2e9e427e35269d8b7087c3ae30f49d2b
hyomin <leehm123456@gmail.com>
2025-05-13 14:59:56 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 18.45 ms, Memory: 38.6 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
@@ -1,10 +1,10 @@
 function dfs(node, graph, visited) {
     visited.add(node);
     let count = 1;
-    for(const neighbor of graph[node] || []) {
-        if(!visited.has(neighbor)) {
-            visited.add(neighbor);
-            count += dfs(neighbor,graph,visited);
+    for (const next of graph[node]) {
+        if(!visited.has(next)) {
+            visited.add(next);
+            count += dfs(next,graph,visited);
         }
     }
     return count;
@@ -13,21 +13,20 @@ function dfs(node, graph, visited) {
 function solution(n, wires) {
     var answer = Infinity;
     
-    
     for (let i = 0; i < wires.length; i++) {
         const visited = new Set();
         const graph = {};
         for (let j = 1; j <= n; j++) graph[j] = [];
         for (let j = 0; j < wires.length; j++) {
             if (i === j) continue;
-            const [u,v] = wires[j];
+            const [u, v] = wires[j];
             graph[u].push(v);
             graph[v].push(u);
         }
-         const first = dfs(1,graph,visited);
-            const second = n - first;
-            answer = Math.min(answer, Math.abs(first-second));
-
+        const first = dfs(1,graph,visited);
+        const second = n - first;
+        answer = Math.min(answer, Math.abs(first-second));
+        
     }
     return answer;
 }
