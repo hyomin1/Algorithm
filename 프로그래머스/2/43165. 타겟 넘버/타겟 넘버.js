8dd789b1f796b7f88abce140070e0b76c625b319
hyomin <leehm123456@gmail.com>
2025-05-13 15:07:59 +0900

    [level 2] Title: 타겟 넘버, Time: 28.66 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,11 +1,12 @@
 function solution(numbers, target) {
     var answer = 0;
-    function dfs(depth,cur) {
+    function dfs(depth, cur) {
         if (depth === numbers.length) {
             return cur === target ? 1 : 0;
         }
-        return dfs(depth + 1, cur + numbers[depth]) + dfs(depth + 1, cur - numbers[depth]);
+        return dfs(depth + 1, cur + numbers[depth]) +  dfs(depth+1, cur - numbers[depth]);
     }
-    answer = dfs(0,0,numbers,target);
+    
+    answer = dfs(0,0);
     return answer;
 }
