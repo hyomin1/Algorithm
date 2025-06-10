4015779646440a019b5c38e4e9c1b9bc41921803
hyomin <leehm123456@gmail.com>
2025-06-10 16:39:05 +0900

    [level 2] Title: 프로세스, Time: 0.47 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,19 +1,18 @@
 function solution(priorities, location) {
     var answer = 0;
-    
     const queue = priorities.map((p,i) => ({p,i}));
-    while (queue.length > 0) {
+    
+    while(queue.length) {
         const current = queue.shift();
         
-        const morePriority = queue.some((v) => current.p < v.p);
-        if(morePriority) {
+        const morePriority = queue.some((q) => current.p < q.p);
+        if (morePriority) {
             queue.push(current);
         } else {
             answer++;
-            if (location === current.i) {
-                return answer;
-            }
+            if (location === current.i) return answer;
         }
     }
+
     return answer;
 }
