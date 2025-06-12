5c73e8fd17740b510e9448bc1c915c3f1ba41fa0
hyomin <leehm123456@gmail.com>
2025-06-12 16:12:22 +0900

    [Silver V] Title: 단어 정렬, Time: 944 ms, Memory: 24004 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const input = fs
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt')
+  .toString()
+  .trim()
+  .replace(/\r/g, '')
+  .split('\n');
+
+const N = parseInt(input[0]);
+
+const words = [...new Set(input.slice(1, N + 1))];
+words.sort((a, b) => {
+  if (a.length === b.length) {
+    return a.localeCompare(b);
+  }
+  return a.length - b.length;
+});
+
+for (const word of words) {
+  console.log(word);
+}
