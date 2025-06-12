141375bab517ee9a35649eca9829cff838a15be5
hyomin <leehm123456@gmail.com>
2025-06-12 15:21:12 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 29.72 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,26 +1,30 @@
-function isValidMove(x,y,n,m,visited,maps) {
-    return x >= 0 && y >= 0 && x < m && y < n && maps[y][x] !== 0 && visited[y][x] === -1;
-}
-
 function solution(maps) {
     var answer = 0;
+    
+    const dx = [0,0,1,-1];
+    const dy = [1,-1,0,0];
     const n = maps.length;
     const m = maps[0].length;
+    
+    const queue = [[0,0]];
     const visited = Array(n).fill().map(() => Array(m).fill(-1));
     visited[0][0] = 1;
-    const queue = [[0,0]];
-    const dx = [1,-1,0,0];
-    const dy = [0,0,1,-1];
+    
+    function isValidMove(x,y) {
+        return x >= 0 && x < m && y >= 0 && y < n && visited[y][x] === -1 && maps[y][x] === 1;
+    }
+    
     while (queue.length) {
-        const [y, x] = queue.shift();
+        const [y,x] = queue.shift();
+        
         for (let i = 0; i < 4; i++) {
             const nx = dx[i] + x;
             const ny = dy[i] + y;
-            if (!isValidMove(nx,ny,n,m,visited,maps)) continue;
-            queue.push([ny,nx]);
+            if(!isValidMove(nx,ny)) continue;
             visited[ny][nx] = visited[y][x] + 1;
+            queue.push([ny,nx]);
         }
     }
-    
-    return visited[n-1][m-1];
+    answer = visited[n-1][m-1];
+    return answer;
 }
