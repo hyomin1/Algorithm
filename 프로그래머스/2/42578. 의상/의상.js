249761a595029077570ac23a650867cda8a58288
hyomin <leehm123456@gmail.com>
2025-06-13 17:53:10 +0900

    [level 2] Title: 의상, Time: 0.32 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -2,14 +2,10 @@ function solution(clothes) {
     var answer = 0;
     const obj = {};
     for (const c of clothes) {
-        const [cloth, type] = c;
-        if(!obj[type]) obj[type] = [];
-        obj[type].push(cloth);
+        const [, type] = c;
+        obj[type] = (obj[type] || 0) + 1;
     }
-    const ans = [];
-    for (const key in obj) {
-        ans.push(obj[key].length + 1); // 안입는 경우 1가지 씩 추가
-    }
+    const ans = Object.values(obj).map((v) => v +1);
     answer = ans.reduce((a,b) => a * b, 1) - 1;
     // 모두 안입는 경우 제외, 한 가지 이상은 무조건 입음
     return answer;
