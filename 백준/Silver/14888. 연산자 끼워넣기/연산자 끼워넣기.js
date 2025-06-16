14a32e099fe5c22f4f097c2b6b50754535365348
hyomin <leehm123456@gmail.com>
2025-06-16 17:25:11 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 156 ms, Memory: 10288 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.js
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.js
@@ -12,39 +12,25 @@ const numbers = input[1].split(' ').map(Number);
 
 const opCounts = input[2].split(' ').map(Number);
 
-const ops = opCounts.flatMap((v, i) => Array(v).fill(op[i]));
-
 let min = Infinity;
 let max = -Infinity;
-const visited = Array(ops.length).fill(false);
-function dfs(depth, cur) {
-  if (depth === numbers.length) {
+
+function dfs(depth, cur, plus, minus, mul, div) {
+  if (depth === N) {
     min = Math.min(min, cur);
     max = Math.max(max, cur);
     return;
   }
-  for (let i = 0; i < ops.length; i++) {
-    if (!visited[i]) {
-      visited[i] = true;
-      switch (ops[i]) {
-        case '+':
-          dfs(depth + 1, cur + numbers[depth]);
-          break;
-        case '-':
-          dfs(depth + 1, cur - numbers[depth]);
-          break;
-        case '*':
-          dfs(depth + 1, cur * numbers[depth]);
-          break;
-        case '/':
-          dfs(depth + 1, ~~(cur/numbers[depth]));
-          break;
-      }
-      visited[i] = false;
-    }
+
+  if (plus > 0) dfs(depth + 1, cur + numbers[depth], plus - 1, minus, mul, div);
+  if (minus > 0)
+    dfs(depth + 1, cur - numbers[depth], plus, minus - 1, mul, div);
+  if (mul > 0) dfs(depth + 1, cur * numbers[depth], plus, minus, mul - 1, div);
+  if (div > 0) {
+    dfs(depth + 1, ~~(cur / numbers[depth]), plus, minus, mul, div - 1);
   }
 }
 
-dfs(1, numbers[0]);
+dfs(1, numbers[0], opCounts[0], opCounts[1], opCounts[2], opCounts[3]);
 console.log(max);
 console.log(min);
