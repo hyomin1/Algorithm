bab9e6fd6f7298e19d69a7d66cbc6eb4b5e6a924
hyomin <leehm123456@gmail.com>
2025-07-16 11:58:50 +0900

    [level 2] Title: N-Queen, Time: 91.36 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.js
+++ 프로그래머스/2/12952. N－Queen/N－Queen.js
@@ -1,25 +1,26 @@
-function search(n,row,width,diagnoal1,diagnoal2) {
-    let answer = 0;
-    if (row === n) {
-        answer++;
-    } else {
-        for (let col = 0; col < n; col++) {
-            if (width[col] || diagnoal1[col+row] || diagnoal2[col-row+n]) {
-                continue;
-            }
-            width[col] = diagnoal1[col+row] = diagnoal2[col-row+n] = true;
-            answer += search(n, row+1,width,diagnoal1,diagnoal2);
-            width[col] = diagnoal1[col+row] = diagnoal2[col-row+n] = false;
-        }
-    }
-    return answer;
-}
-
 function solution(n) {
     var answer = 0;
-    const width = Array(n).fill(false);
-    const diagnoal1 = Array(n*2).fill(false);
-    const diagnoal2 = Array(n*2).fill(false);
-    answer = search(n,0,width,diagnoal1,diagnoal2);
+    const colUsed = Array(n).fill(false);
+    const diag1 = Array(2*n).fill(false);
+    const diag2 = Array(2*n).fill(false);
+    
+    function dfs(row) {
+        if (row === n) {
+            answer++;
+            return;
+        }
+        for (let col = 0; col < n; col++) {
+            if (colUsed[col] || diag1[row-col+n] || diag2[row+col]) continue;
+            colUsed[col] = true;
+            diag1[row-col+n] = true;
+            diag2[row+col] = true;
+            dfs(row+1);
+            
+            colUsed[col] = false;
+            diag1[row-col+n] = false;
+            diag2[row+col] = false;
+        }
+    }
+    dfs(0);
     return answer;
 }
