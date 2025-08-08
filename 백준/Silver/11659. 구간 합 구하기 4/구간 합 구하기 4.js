a2ca36858b5fbe9dfff6aefa5d3fd4e049503c80
hyomin <leehm123456@gmail.com>
2025-08-09 00:38:07 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 332 ms, Memory: 48796 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.js
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.js
@@ -1,21 +1,23 @@
-const fs = require("fs");
-const file = process.platform === "linux" ? "dev/stdin" : "./text.txt";
-const input = fs.readFileSync(file).toString().trim().split("\n");
+const fs = require('fs');
+const input = fs
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt')
+  .toString()
+  .trim()
+  .replace(/\r/g, '')
+  .split('\n');
 
-const [N, M] = input[0].split(" ").map(Number);
-let A = input[1].split(" ").map(Number);
+const [N, M] = input[0].split(' ').map(Number);
+const arr = input[1].split(' ').map(Number);
 
-// 합 배열 S 만들기
-let S = new Array(N + 1).fill(0);
+const S = [];
+S[0] = 0;
 for (let i = 1; i <= N; i++) {
-  S[i] = S[i - 1] + A[i - 1];
+  S[i] = S[i - 1] + arr[i - 1];
+}
+const answer = [];
+for (let i = 2; i < 2 + M; i++) {
+  const [s, e] = input[i].split(' ').map(Number);
+  answer.push(S[e] - S[s - 1]);
 }
 
-let answer = [];
-for (let i = 2; i < M + 2; i++) {
-  const [a, b] = input[i].split(" ").map(Number);
-  // 구간 합 구하기
-  let res = S[b] - S[a - 1];
-  answer.push(res);
-}
-console.log(answer.join("\n"));
+console.log(answer.join('\n'));
