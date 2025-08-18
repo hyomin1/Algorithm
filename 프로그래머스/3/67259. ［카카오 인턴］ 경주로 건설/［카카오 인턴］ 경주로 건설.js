781146fda0815b79c5312d429fa7385735aabcdb
hyomin <leehm123456@gmail.com>
2025-08-18 15:49:55 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 18.41 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.js
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.js
@@ -1,36 +1,75 @@
-function isValidMove(x,y,N,board) {
-    return x >= 0 && y >= 0 && x < N && y < N && board[y][x] === 0;
-}
-
-function calculateCost(prevDirection, direction,cost) {
-    // -1: 최초 도로 건설, 방향 그대로인 경우는 직선도로
-    if (prevDirection === -1 || prevDirection === direction) {
-        return cost + 100;
-    } else return cost + 600;
+class MinHeap {
+    constructor() {
+        this.items = [];
+    }
+    size() {
+        return this.items.length;
+    }
+    push(item) {
+        this.items.push(item);
+        this.bubbleUp();
+    }
+    pop() {
+        if (this.size() === 0) return null;
+        const min = this.items[0];
+        this.items[0] = this.items[this.size() - 1];
+        this.items.pop();
+        this.bubbleDown();
+        return min;
+    }
+    swap(a,b) {
+        [this.items[a],this.items[b]] = [this.items[b], this.items[a]];
+    }
+    bubbleUp() {
+        let index = this.size() - 1;
+        while (index > 0) {
+            const parentIndex = Math.floor((index-1)/2);
+            if (this.items[parentIndex][3] <= this.items[index][3]) break;
+            this.swap(parentIndex,index);
+            index = parentIndex;
+        }
+    }
+    bubbleDown() {
+        let index = 0;
+        while (index * 2 + 1 < this.size()) {
+            const leftChild = index * 2 + 1;
+            const rightChild = index * 2 + 2;
+            const smallerChild = rightChild < this.size() && this.items[rightChild][3] < this.items[leftChild][3] ? rightChild : leftChild;
+            if (this.items[index][3] <= this.items[smallerChild][3]) break;
+            this.swap(index,smallerChild);
+            index = smallerChild;
+        }
+    }
 }
 
 function solution(board) {
-    var answer = Infinity;
+    var answer = 0;
+    
+    const dx = [1,-1,0,0];
+    const dy = [0,0,1,-1];
     const N = board.length;
-    const queue = [];
-    queue.push([0,0,-1,0]); // x, y, direction, cost
-    const dy = [1,-1,0,0]; // 상, 하, 좌, 우
-    const dx = [0,0,-1,1];
+    const dist = Array.from({length:N},()=>Array.from({length:N}, ()=> Array(4).fill(Infinity)));
+    const heap = new MinHeap();
     
-    const visited = Array(N).fill().map(() => Array(N).fill().map(()=>Array(4).fill(0)));
-    
-    while (queue.length > 0) {
-        const [x, y, direction, cost] = queue.shift();
+    for (let i = 0; i < 4; i++) {
+        dist[0][0][i] = 0;
+        heap.push([0,0,i,0]); // y, x , dir, cost
+    }
+    while (heap.size()) {
+        const [y,x,dir,cost] = heap.pop();
+        if (y === N-1 && x === N-1) {
+            return cost;
+        }
+        
         for (let i = 0; i < 4; i++) {
-            const nx = dx[i] + x;
             const ny = dy[i] + y;
-            if(!isValidMove(nx,ny,N,board)) continue;
-            const newCost = calculateCost(direction,i,cost);
-            if (nx === N - 1 && ny === N -1) {
-                answer = Math.min(answer,newCost);
-            } else if (visited[ny][nx][i] === 0 || visited[ny][nx][i] > newCost) {
-                visited[ny][nx][i] = newCost;
-                queue.push([nx,ny,i,newCost]);
+            const nx = dx[i] + x;
+            if (ny < 0 || ny >= N || nx < 0 || nx >= N || board[ny][nx] === 1) continue;
+            
+            const newCost = dir === i ? cost + 100 : cost + 600;
+            if (newCost < dist[ny][nx][i]) {
+                heap.push([ny,nx,i,newCost]);
+                dist[ny][nx][i] = newCost;
             }
         }
     }
