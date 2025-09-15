1ae15939be8f824db2c7088a2ca9ea8639ddcb13
hyomin <leehm123456@gmail.com>
2025-09-15 14:28:38 +0900

    [level 2] Title: 방문 길이, Time: 0.78 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.js
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.js
@@ -1,36 +1,31 @@
-
-function updateLocation(x, y, dir) {
-    switch(dir) {
-        case 'U':
-            return [x, y + 1];
-        case 'D':
-            return [x, y - 1];
-        case 'R':
-            return [x + 1, y];
-        case 'L':
-            return [x - 1, y];
-    }
-}
-
-function isValidMove(x, y) {
-    return x >= -5 && x <= 5 && y >= -5 && y <= 5;
-}
-
 function solution(dirs) {
     var answer = 0;
-    const visited = new Set(); // 중복된 경로 저장
-    
-    let [x, y] = [0, 0];
-    for (const dir of dirs) {
-        const [nx, ny] = updateLocation(x, y, dir);
-        
-        if(!isValidMove(nx, ny)) continue;
-        
-        visited.add(`${x}${y}${nx}${ny}`);
-        visited.add(`${nx}${ny}${x}${y}`);
-        
-        [x, y] = [nx, ny];
+    const command = {
+        'U' : [0,1],
+        'D' : [0,-1],
+        'R' : [1,0],
+        'L' : [-1,0]
     }
-    answer=  visited.size / 2; // 왕복 경로 저장 했으므로 2로 나눈다
+    const set = new Set();
+    function isValid(y,x) {
+        return x >= -5 && x <= 5 && y >= -5 && y <= 5;
+    }
+    const pos = [0,0];
+    for (const dir of dirs) {
+        const nextY = pos[1] + command[dir][1];
+        const nextX = pos[0] + command[dir][0];
+        if (!isValid(nextY,nextX)) continue;
+        const path1 = `${pos[0]}${pos[1]}${nextX}${nextY}`;
+        const path2 = `${nextX}${nextY}${pos[0]}${pos[1]}`;
+        if (!set.has(path1) && !set.has(path2)) {
+            set.add(path1);
+            set.add(path2);
+            answer++;
+        }
+        pos[0] = nextX;
+        pos[1] = nextY;
+    
+    }
+    
     return answer;
 }
