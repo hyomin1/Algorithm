d94909014f15c5ad7ba368bcfd0d0fa309ce3354
hyomin <leehm123456@gmail.com>
2025-09-15 14:38:28 +0900

    [level 2] Title: n^2 배열 자르기, Time: 6.71 ms, Memory: 43.6 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
@@ -1,14 +1,10 @@
 function solution(n, left, right) {
     var answer = [];
-    // 1차원 배열 인덱스에서 n을 나눈 정수 몫이 기존 2차원 배열의 행
-    // 열은 1차원 배열 인덱스에서 n을 나눈 나머지
     for (let i = left; i <= right; i++) {
-        const row = Math.floor(i/n);
+        const row = Math.floor(i / n);
         const col = i % n;
-      
         const max = Math.max(row,col);
         answer.push(max+1);
-        
     }
     return answer;
 }
