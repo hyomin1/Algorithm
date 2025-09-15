db6855822397c3d10a087c070c82fa9eeae98904
hyomin <leehm123456@gmail.com>
2025-09-15 13:31:42 +0900

    [level 1] Title: 모의고사, Time: 27.52 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,26 +1,24 @@
 function solution(answers) {
     var answer = [];
-    
-    const one = [1,2,3,4,5];
-    const two = [2,1,2,3,2,4,2,5];
-    const three = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
-    
-    const score = [0,0,0];
-    
-    
-    answers.forEach((v,i) => {
-        if (one[i%5] === v) score[0]++;
-        if (two[i % 8] === v) score[1]++;
-        if (three[i % 10] === v) score[2]++;
-    })
-    
-    const max = Math.max(...score);
-    
-    score.forEach((v,i)=> {
-        if (v === max) {
-            answer.push(i+1);
+    const person = [[1,2,3,4,5],[2,1,2,3,2,4,2,5],[3,3,1,1,2,2,4,4,5,5]]
+    const obj = {
+        1:0,
+        2:0,
+        3:0
+    }
+    for (let i = 0; i < answers.length; i++) {
+        const answer = answers[i];
+        for (let j = 0; j < person.length; j++) {
+            if (person[j][i % person[j].length] === answer) obj[j+1]++;
         }
-    })
-   
+        
+    }
+    const max = Math.max(...Object.values(obj));
+    
+    
+    for (const key in obj) {
+        if (max === obj[key]) answer.push(Number(key));
+    }
+    answer.sort((a,b)=>a-b);
     return answer;
 }
