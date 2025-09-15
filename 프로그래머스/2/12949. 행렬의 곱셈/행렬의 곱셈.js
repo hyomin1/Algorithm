ec14b91915bce70453adfb13c5e07c6392180fd7
hyomin <leehm123456@gmail.com>
2025-09-15 13:55:52 +0900

    [level 2] Title: 행렬의 곱셈, Time: 26.82 ms, Memory: 40.5 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -1,16 +1,13 @@
 function solution(arr1, arr2) {
-    
     const r1 = arr1.length;
     const c1 = arr1[0].length;
-    const r2 = arr2.length;
     const c2 = arr2[0].length;
-    
-    let answer = Array(r1).fill().map(() => Array(c2).fill(0));
-    
+    const answer = Array.from({length:r1},()=>Array (c2).fill(0));
+    console.log(answer);
     for (let i = 0; i < r1; i++) {
         for (let j = 0; j < c2; j++) {
             for (let k = 0; k < c1; k++) {
-                answer[i][j] += arr1[i][k] * arr2[k][j]; 
+                answer[i][j] += arr1[i][k] * arr2[k][j];
             }
         }
     }
