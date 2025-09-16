16e0092724d02e7d7c6cb790b72c8dc3a9297cf5
hyomin <leehm123456@gmail.com>
2025-09-16 15:53:15 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.44 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -1,22 +1,23 @@
 function solution(board, moves) {
     var answer = 0;
-    const lanes = Array(board.length).fill().map(() => []);
+    const lanes = Array.from({length:board.length}, ()=> []);
+    
     for (let i = board.length - 1; i >= 0; i--) {
         for (let j = 0; j < board[i].length; j++) {
-            const doll = board[i][j];
-            if (doll) lanes[j].push(doll);
+            if (board[i][j]) lanes[j].push(board[i][j]);
         }
     }
-   
+    
     const stack = [];
+    
     for (const move of moves) {
-        const lane = lanes[move-1];
-        if (lane.length > 0) {
-            const doll = lane.pop();
-            if (stack.length > 0 && stack[stack.length - 1] === doll) {
+        if (lanes[move-1].length > 0) { 
+            const doll = lanes[move-1].pop();
+            if (stack.length > 0 && stack[stack.length-1] === doll) {
+                answer+=2;
                 stack.pop();
-                answer += 2;
             } else stack.push(doll);
+            
         }
     }
     return answer;
