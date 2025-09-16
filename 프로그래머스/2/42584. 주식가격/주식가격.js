03ac4a85a9c9d8732021326d4a1b3f1a7872e735
hyomin <leehm123456@gmail.com>
2025-09-16 15:28:33 +0900

    [level 2] Title: 주식가격, Time: 31.37 ms, Memory: 45.1 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,19 +1,18 @@
 function solution(prices) {
     var answer = [];
+    
     const stack = [];
-    const len = prices.length;
-    for (let i = 0; i < len; i++) {
-        while (stack.length > 0 && prices[stack[stack.length - 1]] > prices[i]) {
+    for (let i = 0; i < prices.length; i++) {
+        while (stack.length > 0 && prices[stack[stack.length-1]] > prices[i]) {
             const j = stack.pop();
-            answer[j] = i - j; 
+            answer[j] = i - j;
         }
         stack.push(i);
-        
-    }
-    while (stack.length) {
-        const index = stack.pop();
-        answer[index] = len - index - 1;
     }
     
+    while(stack.length) {
+        const index = stack.pop();
+        answer[index] = prices.length - 1 - index;
+    }
     return answer;
 }
