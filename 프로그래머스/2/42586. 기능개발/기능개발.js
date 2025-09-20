2739b2b64e8a339ed534f379a0b9941b924a111c
hyomin <leehm123456@gmail.com>
2025-09-20 14:59:07 +0900

    [level 2] Title: 기능개발, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -2,12 +2,14 @@ function solution(progresses, speeds) {
     var answer = [];
     const queue = [];
     for (let i = 0; i < progresses.length; i++) {
-        queue.push(Math.ceil((100 - progresses[i]) / speeds[i]));
+        const remain = Math.ceil((100 - progresses[i]) / speeds[i]);
+        queue.push(remain);
     }
+    
     let max = queue[0];
     let count = 0;
     for (let i = 0; i < queue.length; i++) {
-        if (max >= queue[i]) {
+        if (queue[i] <= max) {
             count++;
         } else {
             answer.push(count);
