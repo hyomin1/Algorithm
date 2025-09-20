9ccf5b98d38462b67d71eb15f95fdfd76d6077b5
hyomin <leehm123456@gmail.com>
2025-09-20 15:37:54 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 2013.15 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -1,21 +1,22 @@
-function getWeight(bridge) {
-    return bridge.reduce((sum, val) => sum + val, 0);
-}
-
 function solution(bridge_length, weight, truck_weights) {
     var answer = 0;
-    const bridge = Array(bridge_length).fill(0);
-    
-    while (bridge.length > 0) {
-        bridge.shift();
-        if (truck_weights.length > 0) {
-            const sum = bridge.reduce((a, b) => a + b, 0);
-            if (sum + truck_weights[0] <= weight) {
-                bridge.push(truck_weights.shift());
-            } else bridge.push(0);   
-        }
+    const arrived = [];
+    const queue = [];
+    let index = 0;
+    while (arrived.length !== truck_weights.length) {
         answer++;
+        if (queue.length < bridge_length) {
+            const sum = queue.reduce((acc,cur) => acc + cur,0);
+            if (sum + truck_weights[index] <= weight) {
+                queue.push(truck_weights[index++]);
+            } else queue.push(0);
+        }
+        if (queue.length === bridge_length) {
+            const truck = queue.shift();
+            if (truck !== 0) arrived.push(truck);
+        }
+        
     }
-    
+    answer++;
     return answer;
 }
