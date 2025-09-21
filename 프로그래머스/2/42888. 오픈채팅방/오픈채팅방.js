30a404299f1db3f15d6ba72d6c9d426371991529
hyomin <leehm123456@gmail.com>
2025-09-21 23:21:36 +0900

    [level 2] Title: 오픈채팅방, Time: 303.66 ms, Memory: 89.4 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
@@ -1,18 +1,23 @@
 function solution(record) {
     var answer = [];
-    const obj = {};
-    for (let i = 0; i < record.length; i++) {
-        const [cmd, uid, nickname] = record[i].split(' ');
-        if (cmd === 'Enter' || cmd === 'Change') {
-            obj[uid] = nickname;
+    const user = {};
+    for (const r of record) {
+        const [cmd, uid, name] = r.split(' ');
+        if (name) {
+            user[uid] = name;
         }
     }
-    for (let i = 0; i < record.length; i++) {
-        const [cmd, uid] = record[i].split(' ');
-        if (cmd === 'Enter') {
-            answer.push(`${obj[uid]}님이 들어왔습니다.`)
-        } else if (cmd === 'Leave') answer.push(`${obj[uid]}님이 나갔습니다.`);
-    }
     
+    for (const r of record) {
+        const [cmd, uid] = r.split(' ');
+        switch (cmd) {
+            case 'Enter':
+                answer.push(`${user[uid]}님이 들어왔습니다.`);
+                break;
+            case 'Leave':
+                answer.push(`${user[uid]}님이 나갔습니다.`);
+        }
+        
+    }
     return answer;
 }
