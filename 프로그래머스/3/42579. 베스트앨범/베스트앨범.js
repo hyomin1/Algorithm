3009e934c9cc3ee3f6c4629550b0275cfa2c223f
hyomin <leehm123456@gmail.com>
2025-09-22 15:07:37 +0900

    [level 3] Title: 베스트앨범, Time: 0.42 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -1,24 +1,28 @@
 function solution(genres, plays) {
     var answer = [];
-    const play = {};
-    const genre = {};
-    for (const g of genres) {
-        genre[g] = [];
-    }
-    for (let i = 0; i < genres.length; i++) {
-        genre[genres[i]].push([i,plays[i]]); 
-        play[genres[i]] = (play[genres[i]] || 0) + plays[i];
-    }
     
-    const keys = Object.keys(play).sort((a,b) => play[b] - play[a]);
+    const genre = {};
+    const song = {};
+    for (let i = 0; i < genres.length; i++) {
+        const g = genres[i];
+        genre[g] = (genre[g] || 0) + plays[i];
+        if (!song[g]) song[g] = [];
+        song[g].push([i,plays[i]])
+        
+    }
+   
+    const keys = Object.entries(genre).sort((a,b) => b[1] - a[1]).map((v) => v[0]);
+    
+    
+   
     for (const key of keys) {
-        genre[key].sort((a,b) => {
+        song[key].sort((a,b) => {
             if (a[1] === b[1]) return a[0] - b[0];
             return b[1] - a[1];
         });
-        const arr = genre[key].slice(0,2).map((v) => v[0]);
+        
+        const arr = song[key].slice(0,2).map((v) => v[0]);
         answer.push(...arr);
     }
-    
     return answer;
 }
