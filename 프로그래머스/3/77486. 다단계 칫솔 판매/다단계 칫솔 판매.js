b1250cb4cf263b83cc28fb7ce56b505254afdb99
hyomin <leehm123456@gmail.com>
2025-09-23 14:14:07 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 36.90 ms, Memory: 49.6 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.js
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.js
@@ -1,22 +1,32 @@
 function solution(enroll, referral, seller, amount) {
     var answer = [];
-    let parent = {};
-    let total = {};
-    enroll.forEach((e,i) => {
-        parent[e] = referral[i];
-        total[e] = 0;
-    });
-    for (let i = 0; i < seller.length; i++) {
-        let money = amount[i] * 100;
-        let curName = seller[i];
-        while(money > 0 && curName !== '-') {
-            total[curName] += money - Math.floor(money * 0.1);
-            curName = parent[curName];
-            money = Math.floor(money * 0.1);
-        }
+    const recommender = {};
+    const totalAmount = {};
+    for (let i = 0; i < enroll.length; i++) {
+        const reCommenderName = referral[i];
+        const myName = enroll[i];
+        totalAmount[myName] = 0;
+        if (reCommenderName === '-') continue;
+        recommender[myName] = reCommenderName;
+        
     }
-    answer = Object.values(total);
-   
+    function dfs(name,money) {
+
+        const up = Math.floor(money / 10);
+        const mine = money - up;
+        totalAmount[name] += mine;
+        if (!recommender[name] || up === 0) {
+            return;
+        }
+        dfs(recommender[name],up)
+        
+    }
     
+    for (let i = 0; i < seller.length; i++) {
+        dfs(seller[i],amount[i]*100);
+    }
+
+    
+    answer = Object.values(totalAmount);
     return answer;
 }
