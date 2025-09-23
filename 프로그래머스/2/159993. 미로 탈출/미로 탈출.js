7ae4d8c32495968cecea19748ef11ded67cc6aca
hyomin <leehm123456@gmail.com>
2025-09-23 14:54:05 +0900

    [level 2] Title: 미로 탈출, Time: 47.67 ms, Memory: 38.6 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.js
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.js
@@ -1,50 +1,46 @@
-function isValidMove(x,y,cols,rows,maps) {
-    return x >= 0 && y >= 0 && x < cols && y < rows && maps[y][x] !== 'X';
-}
-
-
 function solution(maps) {
     var answer = 0;
-    let startX = -1, startY = -1;
-    let leverX = -1, leverY = -1;
-    let endX = -1, endY = -1;
-    const rows = maps.length;
-    const cols = maps[0].length;
-    const dy = [-1,1,0,0];
-    const dx = [0,0,-1,1];
-    
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < cols; j++) {
-            if (maps[i][j] === 'S') [startY, startX] = [i, j];
-            if (maps[i][j] === 'L') [leverY, leverX] = [i, j];
-            if (maps[i][j] === 'E') [endY, endX] = [i, j];
+    const N = maps.length;
+    const M = maps[0].length;
+    let s = [];
+    let l = [];
+    let e = [];
+    for (let y = 0; y < N; y++) {
+        for (let x = 0; x < M; x++) {
+            if (maps[y][x] === 'S')  s = [y,x];
+            else if (maps[y][x] === 'L') l = [y,x];
+            else if (maps[y][x] === 'E') e = [y,x];
         }
     }
-    function bfs(x,y,targetX,targetY) {
-        const visited = Array(rows).fill().map(()=>Array(cols).fill(-1));
-        const queue = [[y,x]];
-        visited[y][x] = 0;
-        while(queue.length > 0) {
-            const [y,x] = queue.shift();
-            if (y === targetY && x === targetX) {
-                return visited[y][x];
+    
+    
+    const dirs = [[0,1],[0,-1],[1,0],[-1,0]];
+    function bfs(start,end) {
+        const queue = [[start[0],start[1],0]];
+        const visited = Array.from({length:N}, ()=>Array(M).fill(false));
+        visited[start[0]][start[1]] = true;
+   
+        while (queue.length) {
+            const [y, x, count] = queue.shift();
+            if (y === end[0] && x === end[1]) {
+                return count;
             }
-            for(let i = 0; i < 4; i++) {
-                const nx = dx[i] + x;
-                const ny = dy[i] + y;
-            
-                if (!isValidMove(nx,ny,cols,rows,maps)) continue;
-            
-                if(visited[ny][nx] === -1) {
-                    visited[ny][nx] = visited[y][x] + 1;
-                    queue.push([ny,nx]);
+            for (const dir of dirs) {
+                const [dy, dx] = dir;
+                const ny = dy + y;
+                const nx = dx + x;
+                if (nx >= 0 && nx < M && ny >= 0 && ny < N && !visited[ny][nx] && maps[ny][nx] !== 'X') {
+                    queue.push([ny,nx, count+1]);
+                    visited[ny][nx] = true;
                 }
             }
         }
         return -1;
     }
-    const res1 = bfs(startX,startY,leverX,leverY); // S -> L
-    const res2 = bfs(leverX,leverY,endX,endY); // L -> S
-    if (res1 === -1 || res2 === -1) return -1;
-    return res1 + res2;    
+    const count1 = bfs(s,l);
+    const count2 = bfs(l,e);
+    if (count1 === -1 || count2 === -1) return -1;
+    return count1 + count2;
+     
+   
 }
