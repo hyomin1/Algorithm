5379cadf100a8ab501a47f274c893a884921039f
hyomin <leehm123456@gmail.com>
2025-09-27 14:28:42 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 19.74 ms, Memory: 38.6 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.js
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.js
@@ -1,77 +1,34 @@
-class MinHeap {
-    constructor() {
-        this.items = [];
-    }
-    size() {
-        return this.items.length;
-    }
-    push(item) {
-        this.items.push(item);
-        this.bubbleUp();
-    }
-    pop() {
-        if (this.size() === 0) return null;
-        const min = this.items[0];
-        this.items[0] = this.items[this.size() - 1];
-        this.items.pop();
-        this.bubbleDown();
-        return min;
-    }
-    swap(a,b) {
-        [this.items[a],this.items[b]] = [this.items[b], this.items[a]];
-    }
-    bubbleUp() {
-        let index = this.size() - 1;
-        while (index > 0) {
-            const parentIndex = Math.floor((index-1)/2);
-            if (this.items[parentIndex][3] <= this.items[index][3]) break;
-            this.swap(parentIndex,index);
-            index = parentIndex;
-        }
-    }
-    bubbleDown() {
-        let index = 0;
-        while (index * 2 + 1 < this.size()) {
-            const leftChild = index * 2 + 1;
-            const rightChild = index * 2 + 2;
-            const smallerChild = rightChild < this.size() && this.items[rightChild][3] < this.items[leftChild][3] ? rightChild : leftChild;
-            if (this.items[index][3] <= this.items[smallerChild][3]) break;
-            this.swap(index,smallerChild);
-            index = smallerChild;
-        }
-    }
-}
-
 function solution(board) {
     var answer = 0;
-    
-    const dx = [1,-1,0,0];
-    const dy = [0,0,1,-1];
     const N = board.length;
-    const dist = Array.from({length:N},()=>Array.from({length:N}, ()=> Array(4).fill(Infinity)));
-    const heap = new MinHeap();
+    const visited = Array.from({length: N} ,() => Array.from({length:N} , () => Array(4).fill(Infinity)));
+
     
+    const dirs = [[0,1],[0,-1],[1,0],[-1,0]];
+    const queue = [];
     for (let i = 0; i < 4; i++) {
-        dist[0][0][i] = 0;
-        heap.push([0,0,i,0]); // y, x , dir, cost
+        queue.push([0,0,i,0]);
+        visited[0][0][i] = 0;
     }
-    while (heap.size()) {
-        const [y,x,dir,cost] = heap.pop();
-        if (y === N-1 && x === N-1) {
-            return cost;
-        }
+    while (queue.length) {
+        const [y,x,dir,cost] = queue.shift();
+       
         
         for (let i = 0; i < 4; i++) {
-            const ny = dy[i] + y;
-            const nx = dx[i] + x;
-            if (ny < 0 || ny >= N || nx < 0 || nx >= N || board[ny][nx] === 1) continue;
-            
+            const [dy, dx] = dirs[i];
+            const nx = dx + x;
+            const ny = dy + y;
+            if (ny < 0 || ny >= N || nx < 0 || nx >= N || board[ny][nx] !== 0)
+                continue;
             const newCost = dir === i ? cost + 100 : cost + 600;
-            if (newCost < dist[ny][nx][i]) {
-                heap.push([ny,nx,i,newCost]);
-                dist[ny][nx][i] = newCost;
+            if (newCost < visited[ny][nx][i]) {
+                visited[ny][nx][i] = newCost;
+                queue.push([ny,nx,i,newCost]);
             }
+         
+            
         }
     }
-    return answer;
+    
+    return Math.min(...visited[N-1][N-1]);
 }
