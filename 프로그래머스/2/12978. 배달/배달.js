39f4160ad63d1c30f35d8be9c19fe1df39fc8c70
hyomin <leehm123456@gmail.com>
2025-09-27 14:01:04 +0900

    [level 2] Title: 배달, Time: 6.80 ms, Memory: 35.4 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.js
+++ 프로그래머스/2/12978. 배달/배달.js
@@ -5,18 +5,18 @@ class MinHeap {
     size() {
         return this.items.length;
     }
+    
     push(item) {
         this.items.push(item);
         this.bubbleUp();
     }
     pop() {
-        if (this.size() === 0) {
-            return null;
-        }
+        if (this.size() === 0) return null;
         const min = this.items[0];
         this.items[0] = this.items[this.size() - 1];
         this.items.pop();
         this.bubbleDown();
+        
         return min;
     }
     swap(a,b) {
@@ -26,56 +26,54 @@ class MinHeap {
     bubbleUp() {
         let index = this.size() - 1;
         while (index > 0) {
-            const parentIndex = Math.floor((index-1)/2);
-            if(this.items[parentIndex] <= this.items[index]) {
-                break;
-            }
-            this.swap(parentIndex,index);
+            const parentIndex = Math.floor((index - 1) / 2);
+            if (this.items[parentIndex][1] <= this.items[index][1]) break;
+            
+            this.swap(parentIndex, index);
             index = parentIndex;
         }
     }
     bubbleDown() {
         let index = 0;
         while (index * 2 + 1 < this.size()) {
-            let leftChild = index * 2  + 1;
-            let rightChild = index * 2 + 2;
-            let smallerChild = rightChild < this.size() && this.items[rightChild] < this.items[leftChild] ? rightChild : leftChild;
-            if(this.items[index] <= this.items[smallerChild]) {
-                break;
-            }
-            this.swap(smallerChild,index);
+            const leftChild = index * 2 + 1;
+            const rightChild = index * 2 + 2;
+            const smallerChild = rightChild < this.size() && this.items[rightChild][1] < this.items[leftChild][1] ? rightChild : leftChild;
+            if (this.items[index][1] <= this.items[smallerChild][1]) break;
+            this.swap(index,smallerChild);
             index = smallerChild;
         }
     }
+    
 }
 
 function solution(N, road, K) {
     var answer = 0;
+    const distance = Array.from({length: N+1}).fill(Infinity);
     const graph = {};
-    const distances = Array(N+1).fill(Infinity);
-    distances[1] = 0; 
     for (let i = 1; i <= N; i++) {
         graph[i] = [];
     }
-    for (const [a,b,c] of road) {
-        graph[a].push([b, c]);
-        graph[b].push([a, c]);
+    for (const [u,v,cost] of road) {
+        graph[u].push([v,cost]);
+        graph[v].push([u,cost]);
     }
-    const queue = new MinHeap();
-
-    queue.push([1,distances[1]]); // 1번 출발 , 0
-    while(queue.size() > 0) {
-        const [node, distance] = queue.pop();
+    
+    const heap = new MinHeap();
+    heap.push([1,0]);
+    distance[1] = 0;
+    while (heap.size()) {
+        const [node, cost] = heap.pop();
         
-        for (const [neighbor, nextDistance] of graph[node] || []) {
-            const cost = distance + nextDistance;
-            if(cost < distances[neighbor]) {
-                distances[neighbor] = cost;
-                queue.push([neighbor,cost]);
+        for (const [next, nextCost] of graph[node]) {
+            if (cost + nextCost < distance[next]) {
+                distance[next] = cost + nextCost;
+                heap.push([next,cost+nextCost]);
             }
         }
     }
-    answer = distances.filter((v) => v <= K).length;
+
+    answer = distance.filter((v) => v <= K).length;
 
     return answer;
 }
