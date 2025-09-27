ffb3b2d3b0140975bd6d4271fc713eb659c32e37
hyomin <leehm123456@gmail.com>
2025-09-27 13:40:16 +0900

    [level 3] Title: 네트워크, Time: 0.72 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -4,27 +4,23 @@ function solution(n, computers) {
     for (let i = 0; i < n; i++) {
         graph[i] = [];
     }
+    
     for (let i = 0; i < computers.length; i++) {
         for (let j = 0; j < computers[i].length; j++) {
-            if (i !== j && computers[i][j] === 1) {
+            if (i !== j && computers[i][j] === 1)
                 graph[i].push(j);
-
-            }
+            
         }
     }
-const visited = Array.from({length:n}).fill(false);
-
+    const visited = Array.from({length:n}).fill(false);
     function dfs(node) {
-        if(visited[node]) return;
         visited[node] = true;
         
         for (const next of graph[node]) {
-            if(!visited[next]) {
+            if (!visited[next]) {
                 dfs(next);
             }
         }
-        
-        
     }
     for (let i = 0; i < n; i++) {
         if (!visited[i]) {
@@ -32,5 +28,6 @@ const visited = Array.from({length:n}).fill(false);
             answer++;
         }
     }
+   
     return answer;
 }
