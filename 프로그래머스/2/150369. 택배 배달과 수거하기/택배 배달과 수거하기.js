3a240d817230a5286ae21d675db38c0078bca364
hyomin <leehm123456@gmail.com>
2025-10-03 15:09:14 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 187.81 ms, Memory: 69.6 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.js
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.js
@@ -1,16 +1,42 @@
 function solution(cap, n, deliveries, pickups) {
     var answer = 0;
-    let d = 0, p = 0;
-    for (let i = n - 1; i >= 0; i--) {
-        d += deliveries[i];
-        p += pickups[i];
-        while (d > 0 || p > 0) {
-            d -= cap;
-            p -= cap;
-            answer += (i+1) * 2;
-        }
+    
+    const d = [];
+    const p = [];
+    for (let i = 0; i < deliveries.length; i++) {
+        if (deliveries[i] > 0) d.push([i+1,deliveries[i]]);
+        if (pickups[i] > 0) p.push([i+1,pickups[i]]);
     }
     
-    
+    while (d.length || p.length) {
+        
+        const dDist = d.length > 0 ? d[d.length - 1][0] : 0;
+        const pDist = p.length > 0 ? p[p.length - 1][0] : 0;
+        
+        answer += Math.max(dDist,pDist) * 2;
+        
+        let left = cap;
+        while (left > 0 && d.length > 0) {
+            const [home, count] = d.pop();
+            
+            if (count > left) {
+                d.push([home,count-left]);
+                left = 0;
+            } else {
+                left -= count;
+            }
+        }
+        left = cap;
+        while (left > 0 && p.length > 0) {
+            const [home, count] = p.pop();
+            
+            if (count > left) {
+                p.push([home,count-left]);
+                left = 0;
+            } else left -= count;
+        }
+        
+        
+    }
     return answer;
 }
