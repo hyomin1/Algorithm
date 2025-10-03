a59963c52d95ca30f36ce81d246ec60d53fdf5e5
hyomin <leehm123456@gmail.com>
2025-10-03 14:30:45 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 4.68 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -1,20 +1,35 @@
-function getDay(date) {
-    const [y,m,d] = date.split('.').map(Number);
-    return y * 12 * 28 + m * 28 + d;
-}
-
 function solution(today, terms, privacies) {
     var answer = [];
-    const termMap = {};
+    
+    const term = {};
     for (const t of terms) {
         const [type, month] = t.split(' ');
-        termMap[type] = Number(month) * 28;
+        term[type] = Number(month);
     }
-    const todayDays = getDay(today);
-    privacies.forEach((v,i) => {
-        const [day, type] = v.split(' ');
-        const days = getDay(day) + termMap[type];
-        if (todayDays >= days) answer.push(i+1);
-    })
+    const [year, month, day] = today.split('.').map(Number);
+    
+    for (let i = 0; i < privacies.length; i++) {
+        const p = privacies[i];
+        const [date, type] = p.split(' ');
+        const [pYear, pMonth, pDay] = date.split('.').map(Number);
+        let vYear = pYear + Math.floor((pMonth + term[type]) / 12)
+        let vMonth = (pMonth + term[type]) % 12;
+        if (vMonth === 0) {
+            vYear--;
+            vMonth = 12;
+        }
+        let vDay = pDay - 1;
+        if (vDay === 0) {
+            vMonth--;
+            vDay = 28;
+        }
+        if (vMonth === 0) {
+            vMonth = 12;
+            vYear--;
+        }
+      
+        if (new Date(year,month,day).getTime() > new Date(vYear,vMonth,vDay).getTime()) answer.push(i+1);
+        
+    }
     return answer;
 }
