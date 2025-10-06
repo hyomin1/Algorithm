33bdd64022f9ce71dadd2bb6e9d253d3d25390fe
hyomin <leehm123456@gmail.com>
2025-10-06 19:29:52 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 12.06 ms, Memory: 36.1 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,19 +1,31 @@
 function solution(survey, choices) {
-    const score = [0,3,2,1,0,1,2,3];
-    const info = {'R':0,'T':0,'C':0,'F':0,'J':0,'M':0,'A':0,'N':0};
     var answer = '';
-    for (let i = 0; i < survey.length; i++) {
-        const c = survey[i];
-        const choice = choices[i];
-        if(choices[i] < 4) info[c[0]] += score[choice];
-        else info[c[1]] += score[choice];
+    const obj = {};
+    const arr = ['RT','CF','JM','AN'];
+    for (const element of arr) {
+        const [s1, s2] = element.split('');
+        obj[s1] = 0;
+        obj[s2] = 0;
     }
-    
-    const keys = Object.keys(info);
-    
-    for(let i = 0; i < keys.length; i+=2) {
-        if(info[keys[i]] >= info[keys[i+1]]) answer += keys[i];
-        else answer += keys[i+1];
+    const MID = 4;
+    for (let i = 0; i < survey.length; i++) {
+        const [s1,s2] = survey[i].split('');
+        const choice = choices[i];
+        if (choice < MID) {
+            obj[s1] = obj[s1] + (MID - choice);
+        } else {
+            obj[s2] = obj[s2] + (choice - MID);
+        }
+    }
+    for (const element of arr) {
+        const [s1,s2] = element.split('');
+        if (obj[s1] > obj[s2]) {
+            answer += s1;
+        } else if (obj[s1] < obj[s2]) answer += s2;
+        else {
+            if (s1.localeCompare(s2) < 0) answer += s1;
+            else answer += s2;
+        }
     }
     return answer;
 }
