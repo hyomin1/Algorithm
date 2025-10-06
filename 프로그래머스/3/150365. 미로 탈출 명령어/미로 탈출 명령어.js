c1f67f5bee8d2bdfb6c0e8055cb59b3c744f21c7
hyomin <leehm123456@gmail.com>
2025-10-06 16:14:46 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 634.33 ms, Memory: 95 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.js
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.js
@@ -1,31 +1,36 @@
 function solution(n, m, x, y, r, c, k) {
     var answer = '';
-    const minDistance = Math.abs(x-r) + Math.abs(y-c);
-    if (minDistance > k || (k-minDistance) % 2 !== 0) return 'impossible';
-    const dirs = [
+    
+    const dir = [
         [1,0,'d'],
         [0,-1,'l'],
         [0,1,'r'],
         [-1,0,'u']
     ];
-    
-    function dfs(cx,cy,depth,s) {
+    const visited = Array.from({length:n+1}, () => Array.from({length:m+1}, ()=>Array(k+1).fill(false)));
+    function dfs(depth,cx,cy,path) {
         if (answer) return;
-        const remainingDistance = Math.abs(cx-r) + Math.abs(cy-c);
-        const remainingSteps = k - depth;
-        if (remainingDistance > remainingSteps ||  (remainingSteps - remainingDistance) % 2 !== 0) return;
-        if (depth === k) {
-            if(cx === r && cy === c) answer = s;
+        
+        const remain = k - depth;
+        if (visited[cx][cy][remain]) return;
+        visited[cx][cy][remain] = true;
+        
+        if (remain === 0) {
+            if (cx === r && cy === c) answer = path;
             return;
         }
-        for (const [dx,dy,dir] of dirs) {
+        for (const [dx, dy, d] of dir) {
             const nx = dx + cx;
             const ny = dy + cy;
             if (nx >= 1 && ny >= 1 && nx <= n && ny <= m) {
-                dfs(nx,ny,depth+1,s+dir);
+                dfs(depth+1, nx,ny,path+d);
             }
         }
+        
     }
-    dfs(x,y,0,'');
-    return answer || 'impossible';
+    dfs(0,x,y,'');
+    
+    answer = answer || 'impossible';
+
+    return answer;
 }
