c696251cfd58b90f1f932cb514068357cd7e6fe4
hyomin <leehm123456@gmail.com>
2025-10-08 15:24:45 +0900

    [level 1] Title: 신고 결과 받기, Time: 240.53 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -1,28 +1,27 @@
 function solution(id_list, report, k) {
     var answer = [];
-    const reportedNumber = {};
-    const reportedId = {};
-    for (const r of report) {
-        const [reporter,reported] = r.split(' ');
-        if (!reportedId[reporter]) reportedId[reporter] = [];
-        if (!reportedId[reporter].includes(reported)) {
-            reportedId[reporter].push(reported);
-            reportedNumber[reported] = (reportedNumber[reported] || 0)+1;
-        }
-        
-    }
-    const stopped = Object.entries(reportedNumber).filter((v) => v[1] >= k).map((v) => v[0]);
-
+    const reportList = {};
+    const reportedNum = {};
     for (const id of id_list) {
+        reportList[id] = new Set();
+        reportedNum[id] = 0;
+    }
+    for (const r of report) {
+        const [u, v] = r.split(' ');
+        reportList[u].add(v);
+    }
+    for (const u in reportList) {
+        for (const v of reportList[u]) {
+            reportedNum[v]++;
+        }
+    }
+    let idx = 0;
+    for (const u in reportList) {
         let count = 0;
-        if (!reportedId[id]) {
-            answer.push(count);
-            continue;
+        for (const v of reportList[u]) {
+            if (reportedNum[v] >= k) count++;
         }
-        for (let i = 0; i < reportedId[id].length; i++) {
-            if(stopped.includes(reportedId[id][i])) count++;
-        }
-        answer.push(count);
+        answer[idx++] = count;
         
     }
     return answer;
