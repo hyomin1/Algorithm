f4a2119ce67c5db0b227a88b14a1c61266c418d6
hyomin <leehm123456@gmail.com>
2025-10-09 19:12:29 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 102.43 ms, Memory: 180 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.js
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.js
@@ -1,32 +1,32 @@
 function solution(board, skill) {
     var answer = 0;
-    const n = board.length;
-    const m = board[0].length;
-    const S = Array.from({length:n+1},() => Array.from({length:m+1}).fill(0));
-    for (const s of skill) {
-        const [type,r1,c1,r2,c2,degree] = s;
+    const N = board.length;
+    const M = board[0].length;
+    const arr = Array.from({length:N+1}, ()=>Array(M+1).fill(0));
+    
+    for (const [type, r1, c1, r2, c2, degree] of skill) {
         const value = type === 1 ? -degree : degree;
-        S[r1][c1] += value;
-        S[r1][c2+1] += -value;
-        S[r2+1][c1] += -value;
-        S[r2+1][c2+1] += value;
-        
+        arr[r1][c1] += value;
+        arr[r1][c2+1] -= value;
+        arr[r2+1][c1] -= value;
+        arr[r2+1][c2+1] += value;
     }
-    for (let i = 0; i <= n; i++) {
-        for (let j = 0; j < m; j++) {
-            S[i][j+1] += S[i][j];
+    
+    for (let i = 0; i <= N; i++) {
+        for (let j = 1; j <= M; j++) {
+            arr[i][j] += arr[i][j-1];
         }
     }
-    for (let j = 0; j <= m; j++) {
-        for (let i = 0; i < n; i++) {
-            S[i+1][j] += S[i][j];
+    for (let i = 0; i <= M; i++) {
+        for (let j = 1; j <= N; j++) {
+            arr[j][i] += arr[j-1][i];
         }
     }
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < m; j++) {
-            if (board[i][j] + S[i][j] > 0) answer++;
+    
+    for (let i = 0; i < N; i++) {
+        for (let j = 0; j < M; j++) {
+            if (board[i][j] + arr[i][j] > 0) answer++;
         }
     }
-   
     return answer;
 }
