76a4f21f0430723499deb8c84f30ef1bb9182c18
hyomin <leehm123456@gmail.com>
2025-11-13 14:01:24 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 23.33 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.js
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.js
@@ -1,4 +1,4 @@
-class MinHeap {
+class Heap {
     constructor() {
         this.items = [];
     }
@@ -11,67 +11,71 @@ class MinHeap {
     }
     pop() {
         if (this.size() === 0) return null;
-        const min = this.items[0];
+        const item = this.items[0];
         this.items[0] = this.items[this.size() - 1];
         this.items.pop();
         this.bubbleDown();
-        return min;
+        
+        return item;
     }
-    compare(a,b) { // 0 : 소요시간 1 : 요청시간 2: 작업번호
-        if (a[0] !== b[0]) return a[0] - b[0];
+    swap(a,b) {
+        [this.items[a], this.items[b]] = [this.items[b], this.items[a]];
+    }
+    compare(a,b) {
         if (a[1] !== b[1]) return a[1] - b[1];
+        if (a[0] !== b[0]) return a[0] - b[0];
         return a[2] - b[2];
     }
     swap(a,b) {
-        [this.items[a], this.items[b]] = [this.items[b] , this.items[a]];
+        [this.items[a], this.items[b]] = [this.items[b], this.items[a]];
     }
-    
     bubbleUp() {
-        let index = this.size() - 1;
-        while (index > 0) {
-            const parentIndex = Math.floor((index-1)/2);
-            if (this.compare(this.items[parentIndex],this.items[index]) <= 0) break;
-            this.swap(parentIndex,index);
-            index = parentIndex;
+        let i = this.size() - 1;
+        while (i > 0) {
+            const p = Math.floor((i-1)/2);
+            if (this.compare(this.items[p],this.items[i]) <= 0) break;
+            this.swap(i,p);
+            i = p;
         }
     }
     bubbleDown() {
-        let index = 0;
-        while (index * 2 + 1 < this.size()) {
-            const leftChild = index * 2 + 1;
-            const rightChild = index * 2 + 2;
-            const smallerChild = rightChild < this.size() && this.compare(this.items[rightChild],this.items[leftChild]) <= 0 ? rightChild : leftChild;
-            if (this.compare(this.items[index],this.items[smallerChild]) <= 0) break;
-            this.swap(index,smallerChild);
-            index = smallerChild;
+        let i = 0;
+        while (i * 2 + 1 < this.size()) {
+            const l = i * 2 + 1;
+            const r = i * 2 + 2;
+            const s = r < this.size() && this.compare(this.items[r],this.items[l]) <= 0 ? r : l;
+            if (this.compare(this.items[i],this.items[s]) <= 0) break;
+            this.swap(i,s);
+            i = s;
         }
     }
 }
 
-
 function solution(jobs) {
     var answer = 0;
-    jobs.sort((a,b)=>a[0]-b[0]);
-    const heap = new MinHeap();
-    let count = 0;
-    let curTime = 0;
-    let index = 0;
-    let totalTime = 0;
-    while (count < jobs.length) {
-        while (index < jobs.length && jobs[index][0] <= curTime) {
-            const [reqTime,duration] = jobs[index];
-            heap.push([duration,reqTime,index]);
-            index++;
+    jobs.sort((a,b) => a[0] - b[0]);
+    
+    let work = 0;
+    let i = 0;
+    let time = 0;
+    
+    const heap = new Heap();
+    while (work < jobs.length) {
+        while (i < jobs.length && jobs[i][0] <= time) {
+            heap.push([...jobs[i],i]);
+            i++;
         }
-        if (heap.size() > 0) {
-            const [duration,reqTime] = heap.pop();
-            curTime += duration;
-            totalTime += curTime - reqTime;
-            count++;
+        
+        if (heap.size()) {
+            const [reqTime, duration] = heap.pop();
+            time += duration;
+            answer += time - reqTime;
+            work++;
         } else {
-            curTime = jobs[index][0];
+            time = jobs[i][0];
         }
     }
-    answer = Math.floor(totalTime/jobs.length);
-    return answer;
+    
+    
+    return Math.floor(answer/jobs.length);
 }
