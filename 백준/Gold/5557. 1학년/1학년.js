6cc0bbc24522e10e9c53f2ae5d915a4ee3c56420
hyomin <leehm123456@gmail.com>
2025-11-14 14:34:05 +0900

    [Gold V] Title: 1학년, Time: 92 ms, Memory: 9368 KB -BaekjoonHub

---
+++ 백준/Gold/5557. 1학년/1학년.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const input = fs
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt')
+  .toString()
+  .trim()
+  .replace(/\r/g, '')
+  .split('\n');
+
+const N = parseInt(input[0]);
+
+const arr = input[1].split(' ').map(Number);
+
+const res = arr[arr.length - 1];
+
+const dp = Array.from({ length: N }, () => Array(21).fill(0n));
+
+if (arr[0] >= 0 && arr[0] <= 20) {
+  dp[0][arr[0]] = 1n;
+}
+
+for (let i = 1; i <= N - 2; i++) {
+  const val = arr[i];
+  for (let j = 0; j <= 20; j++) {
+    if (dp[i - 1][j] > 0n) {
+      if (j - val >= 0) dp[i][j - val] += dp[i - 1][j];
+      if (j + val <= 20) dp[i][j + val] += dp[i - 1][j];
+    }
+  }
+}
+
+console.log(dp[N - 2][res].toString());
