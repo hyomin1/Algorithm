2bb69cc25c51149981aa23a77523b6b410a7d7e5
hyomin <leehm123456@gmail.com>
2025-11-26 14:45:50 +0900

    [Gold II] Title: 트리의 지름, Time: 912 ms, Memory: 108528 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.js
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.js
@@ -8,40 +8,45 @@ const input = fs
 
 const V = parseInt(input[0]);
 
-const info = input.slice(1).map((l) => l.split(' ').map(Number));
-
 const graph = Array.from({ length: V + 1 }, () => []);
 
-for (const element of info) {
-  const v = element[0];
-  for (let j = 1; j < element.length - 1; j += 2) {
-    graph[v].push([element[j], element[j + 1]]);
+for (let i = 1; i <= V; i++) {
+  const arr = input[i].split(' ').map(Number).slice(0, -1);
+
+  const node = arr[0];
+
+  for (let j = 1; j < arr.length; j += 2) {
+    graph[node].push([arr[j], arr[j + 1]]);
   }
 }
 
+// 지름 : 임의의 노드에서 가정 먼점에서 먼점
 const visited = Array.from({ length: V + 1 }).fill(-1);
-
 function dfs(node, cost, visited) {
   visited[node] = cost;
+
   for (const [next, nextCost] of graph[node]) {
     if (visited[next] === -1) {
-      visited[next] = cost + nextCost;
-      dfs(next, cost + nextCost, visited);
+      const newCost = cost + nextCost;
+      visited[next] = newCost;
+      dfs(next, newCost, visited);
     }
   }
 }
-let start = 1;
-dfs(start, 0, visited);
+dfs(1, 0, visited);
 
 const max = Math.max(...visited);
 
-for (let i = 2; i < visited.length; i++) {
-  if (visited[i] === max) {
+let start = 0;
+for (let i = 1; i < visited.length; i++) {
+  if (max === visited[i]) {
     start = i;
     break;
   }
 }
 const visited2 = Array.from({ length: V + 1 }).fill(-1);
-dfs(start, 0, visited2);
 
-console.log(Math.max(...visited2));
+dfs(start, 0, visited2);
+const answer = Math.max(...visited2);
+
+console.log(answer);
