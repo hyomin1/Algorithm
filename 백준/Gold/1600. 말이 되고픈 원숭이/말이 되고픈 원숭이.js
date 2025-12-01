b1f7873c21a85b508635bd130eea0b4cccf165f8
hyomin <leehm123456@gmail.com>
2025-12-01 14:52:33 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 920 ms, Memory: 187840 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.js
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.js
@@ -7,64 +7,65 @@ const input = fs
   .split('\n');
 
 const K = parseInt(input[0]);
+
 const [W, H] = input[1].split(' ').map(Number);
-const board = input.slice(2).map((line) => line.split(' ').map(Number));
+const board = input.slice(2).map((v) => v.split(' ').map(Number));
 
-const queue = [[0, 0, 0, 0]]; // y, x, count, k체크
+const queue = [[0, 0, 0, 0]];
 
-const dir = [
+const dirs = [
   [0, 1],
   [0, -1],
   [1, 0],
   [-1, 0],
 ];
-
 const horse = [
-  [-1, 2],
-  [-2, 1],
-  [-2, -1],
   [-1, -2],
-  [1, -2],
-  [2, -1],
-  [2, 1],
+  [-2, -1],
+  [-2, 1],
+  [-1, 2],
   [1, 2],
+  [2, 1],
+  [2, -1],
+  [1, -2],
 ];
 
-const visited = Array.from({ length: H }, () =>
-  Array.from({ length: W }, () => Array.from(K + 1))
-);
-visited[0][0][0] = true;
+let front = 0;
 
-function isValid(y, x) {
-  return y >= 0 && y < H && x >= 0 && x < W && board[y][x] !== 1;
-}
-while (queue.length) {
-  const [y, x, count, k] = queue.shift();
+const visited = Array.from({ length: H }, () =>
+  Array.from({ length: W }, () => Array(K + 1).fill(false))
+);
+
+while (front < queue.length) {
+  const [y, x, count, k] = queue[front++];
 
   if (y === H - 1 && x === W - 1) {
     console.log(count);
     process.exit(0);
   }
 
+  for (const [dy, dx] of dirs) {
+    const ny = dy + y;
+    const nx = dx + x;
+
+    if (ny < 0 || ny >= H || nx < 0 || nx >= W) continue;
+    if (board[ny][nx] === 1 || visited[ny][nx][k]) continue;
+
+    queue.push([ny, nx, count + 1, k]);
+    visited[ny][nx][k] = true;
+  }
+
   if (k < K) {
     for (const [dy, dx] of horse) {
-      const nx = dx + x;
       const ny = dy + y;
-      if (!isValid(ny, nx)) continue;
-      if (!visited[ny][nx][k + 1]) {
-        queue.push([ny, nx, count + 1, k + 1]);
-        visited[ny][nx][k + 1] = true;
-      }
-    }
-  }
-  for (const [dy, dx] of dir) {
-    const nx = dx + x;
-    const ny = dy + y;
-    if (!isValid(ny, nx)) continue;
-    if (!visited[ny][nx][k]) {
-      queue.push([ny, nx, count + 1, k]);
-      visited[ny][nx][k] = true;
+      const nx = dx + x;
+      if (ny < 0 || ny >= H || nx < 0 || nx >= W) continue;
+      if (board[ny][nx] === 1 || visited[ny][nx][k + 1]) continue;
+
+      queue.push([ny, nx, count + 1, k + 1]);
+      visited[ny][nx][k + 1] = true;
     }
   }
 }
+
 console.log(-1);
