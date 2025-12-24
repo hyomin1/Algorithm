ef3248770ab30f02598ebed2c99cf839fcc6ec47
hyomin <leehm123456@gmail.com>
2025-12-24 14:47:58 +0900

    [Gold I] Title: 멀티탭 스케줄링, Time: 88 ms, Memory: 9364 KB -BaekjoonHub

--- 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.js
+++ 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.js
@@ -11,11 +11,12 @@ const [N, K] = input[0].split(' ').map(Number);
 const arr = input[1].split(' ').map(Number);
 
 const plug = [];
+
 let answer = 0;
+
 for (let i = 0; i < K; i++) {
   const now = arr[i];
-
-  if (plug.includes(now)) continue; // 이미 꽂혀 있음
+  if (plug.includes(now)) continue;
 
   if (plug.length < N) {
     plug.push(now);
@@ -24,8 +25,10 @@ for (let i = 0; i < K; i++) {
 
   let remove = -1;
   let max = -1;
+
   for (const p of plug) {
     let next = Infinity;
+
     for (let j = i + 1; j < K; j++) {
       if (arr[j] === p) {
         next = j;
@@ -36,13 +39,11 @@ for (let i = 0; i < K; i++) {
       remove = p;
       break;
     }
-
     if (next > max) {
       max = next;
       remove = p;
     }
   }
-
   const index = plug.indexOf(remove);
   plug.splice(index, 1);
   plug.push(now);
